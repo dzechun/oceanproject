3eb9601ac23bb4616a4a6b2cc62611c6f7c6f60a
霖 <2226190315@qq.com>
2020-09-08 17:16:14 +0800

    初始化

---
+++ ocean-common/ocean-common-base/ocean-public-base/src/main/java/com/fantechs/common/base/utils/CurrentUserInfoUtils.java
@@ -0,0 +1,40 @@
+package com.fantechs.common.base.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fantechs.common.base.constants.ErrorCodeEnum;
+import com.fantechs.common.base.exception.BizErrorException;
+import com.fantechs.common.base.exception.TokenValidationFailedException;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CurrentUserInfoUtils {
+    private static RedisUtil redisUtil=(RedisUtil) SpringUtil.getBean("redisUtil");
+
+    public static String getToken() throws TokenValidationFailedException, BizErrorException {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if(StringUtils.isEmpty(requestAttributes)){
+            throw new BizErrorException(ErrorCodeEnum.GL9999404,"未找到请求");
+        }
+        HttpServletRequest request = requestAttributes.getRequest();
+        String token = request.getHeader("token");
+        if(StringUtils.isEmpty(token)){
+            throw new TokenValidationFailedException(ErrorCodeEnum.UAC10011039);
+        }
+        return token;
+    }
+//    /**
+//     * 获取当前登录用户信息
+//     * @return
+//     */
+//    public static User getCurrentUserInfo() throws TokenValidationFailedException, BizErrorException {
+//        String token = getToken();
+//        Object o= redisUtil.get(token);
+//        if(StringUtils.isEmpty(o)){
+//            throw new TokenValidationFailedException(ErrorCode.USER_TOKEN_ERROR);
+//        }
+//        User user = JSONObject.parseObject(JSONObject.toJSONString(o),User.class);
+//        return user;
+//    }
+}
