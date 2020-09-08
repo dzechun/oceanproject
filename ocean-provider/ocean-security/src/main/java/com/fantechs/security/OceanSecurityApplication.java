bc931c1c8f3f48968c02a7838daebe996a75faa8
霖 <2226190315@qq.com>
2020-09-08 17:37:40 +0800

    初始化

---
+++ ocean-provider/ocean-security/src/main/java/com/fantechs/security/OceanSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.fantechs.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanSecurityApplication.class, args);
+    }
+
+}
