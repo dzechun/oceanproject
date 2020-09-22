3863f14cf460b3b58910fa831c578d8e7bdefb18
霖 <2226190315@qq.com>
2020-09-22 09:41:51 +0800

    优化

--- ocean-provider/ocean-security/src/main/java/com/fantechs/security/service/impl/UserDetailsServiceImpl.java
+++ ocean-provider/ocean-security/src/main/java/com/fantechs/security/service/impl/UserDetailsServiceImpl.java
@@ -45,7 +45,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         Example example = new Example(SysUser.class);
         Example.Criteria criteria = example.createCriteria();
-        criteria.andEqualTo("status",1).andEqualTo("userName", s);
+        criteria.andEqualTo("status",1).andEqualTo("userName", s).orEqualTo("userCode",s);
         SysUser user = sysUserMapper.selectOneByExample(example);
         if(StringUtils.isEmpty(user)){
             throw new BizErrorException(ErrorCodeEnum.UAC10011011,s);
