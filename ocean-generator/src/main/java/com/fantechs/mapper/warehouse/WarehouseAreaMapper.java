aaaa48fc3674910c727f97260041adffccdeee46
wangcaozhou <2049579718@qq.com>
2020-09-23 16:54:07 +0800

    企业基础资料功能

--- ocean-generator/src/main/java/com/fantechs/mapper/warehouse/WarehouseAreaMapper.java
+++
@@ -1,7 +0,0 @@
-package com.fantechs.mapper.warehouse;
-
-import com.fantechs.common.base.mybatis.MyMapper;
-import com.fantechs.model.warehouse.WarehouseArea;
-
-public interface WarehouseAreaMapper extends MyMapper<WarehouseArea> {
-}
