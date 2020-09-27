3d6ca2a079d4adeadc90fd3ab49acbfd529b7174
雷秦林 <lql@fantechs.com.cn>
2020-09-27 15:12:10 +0800

    供应商信息提交

---
+++ ocean-provider/ocean-imes/ocean-imes-basic/src/main/java/com/fantechs/provider/imes/basic/service/impl/SmtSupplierServiceImpl.java
@@ -0,0 +1,88 @@
+package com.fantechs.provider.imes.basic.service.impl;
+
+import com.fantechs.common.base.constants.ErrorCodeEnum;
+import com.fantechs.common.base.entity.basic.SmtSupplier;
+import com.fantechs.common.base.entity.basic.search.SearchSmtSupplier;
+import com.fantechs.common.base.entity.security.SysUser;
+import com.fantechs.common.base.exception.BizErrorException;
+import com.fantechs.common.base.support.BaseService;
+import com.fantechs.common.base.utils.CurrentUserInfoUtils;
+import com.fantechs.common.base.utils.StringUtils;
+import com.fantechs.provider.imes.basic.mapper.SmtSupplierMapper;
+import com.fantechs.provider.imes.basic.service.SmtSupplierService;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * Created by Mr.Lei on 2020/09/27.
+ */
+@Service
+public class SmtSupplierServiceImpl  extends BaseService<SmtSupplier> implements SmtSupplierService {
+
+    @Resource
+    private SmtSupplierMapper smtSupplierMapper;
+
+    @Override
+    public List<SmtSupplier> findList(SearchSmtSupplier searchSmtSupplier) {
+        return smtSupplierMapper.findList(searchSmtSupplier);
+    }
+
+    @Override
+    public int save(SmtSupplier record) {
+        SysUser currentUser = CurrentUserInfoUtils.getCurrentUserInfo();
+        if(StringUtils.isEmpty(currentUser)){
+            throw new BizErrorException(ErrorCodeEnum.UAC10011039);
+        }
+        Example example = new Example(SmtSupplier.class);
+        example.createCriteria().andEqualTo("supplier_code",record.getSupplierCode());
+        List<SmtSupplier> list = smtSupplierMapper.selectByExample(example);
+        if(list!=null && list.size()>0){
+            throw new BizErrorException(ErrorCodeEnum.OPT20012001);
+        }
+        record.setCreateTime(new Date());
+        record.setCreateUserId(currentUser.getUserId());
+        int i = smtSupplierMapper.insertSelective(record);
+        return i;
+    }
+
+    @Override
+    public int update(SmtSupplier entity) {
+        SysUser currentUser = CurrentUserInfoUtils.getCurrentUserInfo();
+        if(StringUtils.isEmpty(currentUser)){
+            throw new BizErrorException(ErrorCodeEnum.UAC10011039);
+        }
+        Example example = new Example(SmtSupplier.class);
+        example.createCriteria().andEqualTo("supplier_code",entity.getSupplierCode());
+        List<SmtSupplier> list = smtSupplierMapper.selectByExample(example);
+        if(list!=null && list.size()>0){
+            throw new BizErrorException(ErrorCodeEnum.OPT20012001);
+        }
+        entity.setModifiedTime(new Date());
+        entity.setModifiedUserId(currentUser.getUserId());
+        int i = smtSupplierMapper.updateByPrimaryKeySelective(entity);
+        return i;
+    }
+
+    @Override
+    public int batchDelete(String ids) {
+        SysUser currentUser = CurrentUserInfoUtils.getCurrentUserInfo();
+        if(StringUtils.isEmpty(currentUser)){
+            throw new BizErrorException(ErrorCodeEnum.UAC10011039);
+        }
+        int i = 0;
+        String[] idsArr = ids.split(",");
+        for (String item:idsArr) {
+            SmtSupplier smtSupplier = smtSupplierMapper.selectByPrimaryKey(item);
+            if(StringUtils.isEmpty(smtSupplier)){
+                throw new BizErrorException(ErrorCodeEnum.OPT20012003);
+            }
+            i = smtSupplierMapper.deleteByPrimaryKey(item);
+        }
+        return super.batchDelete(ids);
+    }
+}
