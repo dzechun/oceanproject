02a98d94ab000ae551d69bb15f40be4932ec0c8d
wangcaozhou <2049579718@qq.com>
2020-09-28 14:55:29 +0800

    产品基础资料功能

--- ocean-provider-api/ocean-security-api/src/main/java/com/fantechs/provider/api/security/service/LoginFeignApi.java
+++ ocean-provider-api/ocean-security-api/src/main/java/com/fantechs/provider/api/security/service/LoginFeignApi.java
@@ -1,8 +1,12 @@
 package com.fantechs.provider.api.security.service;
 
 
+import com.fantechs.common.base.entity.security.SysUser;
 import com.fantechs.common.base.response.ResponseEntity;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,5 +19,9 @@ public interface LoginFeignApi {
 
     @PostMapping(value = "/login")
     ResponseEntity login(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password);
+
+    @GetMapping("/sysUser/detail")
+    @ApiOperation(value = "通过ID获取单个用户信息",notes = "通过ID获取单个用户信息")
+    ResponseEntity<SysUser> selectUserById(@ApiParam(value = "传入主键userId", required = true) @RequestParam Long id);
 
 }
