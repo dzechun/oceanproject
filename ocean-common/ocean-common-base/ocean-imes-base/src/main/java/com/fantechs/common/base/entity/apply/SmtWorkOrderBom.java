14c7da9f06e52f1739432e75cc63fea3ffffecbf
wangcaozhou <2049579718@qq.com>
2020-10-14 15:33:41 +0800

    应用程序功能

--- ocean-common/ocean-common-base/ocean-imes-base/src/main/java/com/fantechs/common/base/entity/apply/SmtWorkOrderBom.java
+++ ocean-common/ocean-common-base/ocean-imes-base/src/main/java/com/fantechs/common/base/entity/apply/SmtWorkOrderBom.java
@@ -4,9 +4,7 @@ import com.fantechs.common.base.support.ValidGroup;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -23,6 +21,7 @@ public class SmtWorkOrderBom extends ValidGroup implements Serializable {
     @Column(name = "work_order_bom_id")
     @ApiModelProperty(name="workOrderBomId" ,value="工单BOM ID")
     @NotNull(groups = ValidGroup.update.class,message = "工单BOM ID不能为空")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long workOrderBomId;
 
     /**
