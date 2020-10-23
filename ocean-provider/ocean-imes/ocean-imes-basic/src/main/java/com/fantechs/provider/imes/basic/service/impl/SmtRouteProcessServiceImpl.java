1284faeafa5b96693ef20e4804d0cb9f378bcf6f
wangcaozhou <2049579718@qq.com>
2020-10-23 16:24:57 +0800

    工艺路线配置修改

--- ocean-provider/ocean-imes/ocean-imes-basic/src/main/java/com/fantechs/provider/imes/basic/service/impl/SmtRouteProcessServiceImpl.java
+++ ocean-provider/ocean-imes/ocean-imes-basic/src/main/java/com/fantechs/provider/imes/basic/service/impl/SmtRouteProcessServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -50,25 +51,60 @@ public class SmtRouteProcessServiceImpl extends BaseService<SmtRouteProcess> imp
             criteria.andEqualTo("routeId",routeId);
             smtRouteProcessMapper.deleteByExample(example);
 
-            for (SmtRouteProcess smtRouteProcess : list) {
+            List<Long> processIds=new ArrayList<>();
+            for (int i=0;i<list.size();i++) {
+                SmtRouteProcess smtRouteProcess = list.get(i);
                 Long processId = smtRouteProcess.getProcessId();
                 Long nextProcessId = smtRouteProcess.getNextProcessId();
+                Long previousProcessId = smtRouteProcess.getPreviousProcessId();
                 Integer orderNum = smtRouteProcess.getOrderNum();
-                SmtProcess smtProcess = smtProcessMapper.selectByPrimaryKey(processId);
 
+                processIds.add(processId);
+                //当前工序
+                SmtProcess smtProcess = smtProcessMapper.selectByPrimaryKey(processId);
                 if(StringUtils.isNotEmpty(smtProcess)){
                     //查询当前工序的工序类别
                     SmtProcessCategory smtProcessCategory = smtProcessCategoryMapper.selectByPrimaryKey(smtProcess.getProcessCategoryId());
-                    if(!smtProcessCategory.getProcessCategoryCode().equalsIgnoreCase("repair")){
+                    if(StringUtils.isNotEmpty(smtProcessCategory)&&!smtProcessCategory.getProcessCategoryCode().equalsIgnoreCase("repair")){
                         if(StringUtils.isEmpty(orderNum)){
                             throw new BizErrorException("非维修工序的工序顺序不能为空");
                         }
-
-                        if(smtRouteProcess.getIsPass()==0){
-                            throw new BizErrorException("非维修工序的工序是否通过不能为否");
-                        }
                     }
 
+                    if(list.size()>1){
+                        if(i>0){
+                            if(previousProcessId.equals(list.get(i-1).getPreviousProcessId())){
+                                //上一道工序相同的两条数据的当前工序为检验的工序或维修工序
+                                if(StringUtils.isNotEmpty(nextProcessId)){
+                                    //下一道工序
+                                    SmtProcess nextProcess = smtProcessMapper.selectByPrimaryKey(processId);
+                                    if(StringUtils.isNotEmpty(nextProcess)){
+                                        //查询当前工序的下一道工序的工序类别
+                                        SmtProcessCategory nextProcessCategory = smtProcessCategoryMapper.selectByPrimaryKey(nextProcess.getProcessCategoryId());
+                                        //判断当前数据的下一道工序是否是维修工序
+                                        if(StringUtils.isNotEmpty(nextProcessCategory)&&nextProcessCategory.getProcessCategoryCode().equalsIgnoreCase("repair")){
+                                            if(!processIds.contains(list.get(i+1).getNextProcessId())){
+                                                throw new BizErrorException("维修后，不能执行当前工序以后的工序");
+                                            }
+                                        }else {
+                                            if(previousProcessId.equals(list.get(i-1).getPreviousProcessId())){
+                                                continue;
+                                            }else {
+                                                throw new BizErrorException("工艺路线配置错误");
+                                            }
+                                        }
+                                    }
+                                }
+                            }else {
+                                //当前数据的工序和上一条数据的下一道工序相同并且当前工序顺序大于上一条数据的工序顺序才能通过
+                                if(processId.equals(list.get(i-1).getNextProcessId())){
+                                    continue;
+                                }else {
+                                    throw new BizErrorException("工艺路线配置错误");
+                                }
+                            }
+                        }
+                    }
                 }
             }
 
