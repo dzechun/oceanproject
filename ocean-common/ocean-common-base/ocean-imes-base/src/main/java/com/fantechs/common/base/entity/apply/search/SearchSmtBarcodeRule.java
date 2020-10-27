5601fea7eeee0dd6bdc644d395b7d1dff3aaebd8
wangcaozhou <2049579718@qq.com>
2020-10-27 16:21:40 +0800

    条码规则功能

--- ocean-common/ocean-common-base/ocean-imes-base/src/main/java/com/fantechs/common/base/entity/apply/search/SearchSmtBarcodeRule.java
+++ ocean-common/ocean-common-base/ocean-imes-base/src/main/java/com/fantechs/common/base/entity/apply/search/SearchSmtBarcodeRule.java
@@ -1,12 +1,9 @@
 package com.fantechs.common.base.entity.apply.search;
 
-import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.fantechs.common.base.dto.BaseQuery;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
