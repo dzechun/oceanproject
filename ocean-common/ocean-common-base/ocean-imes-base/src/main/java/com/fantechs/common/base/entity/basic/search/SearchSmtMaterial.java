6aea2a8fd26017d8c6bfaf9782af5b3fd03ed6fc
wangcaozhou <2049579718@qq.com>
2020-10-29 14:06:18 +0800

    产品BOM明细和工单BOM选择零件料号时，过滤掉产品料号

--- ocean-common/ocean-common-base/ocean-imes-base/src/main/java/com/fantechs/common/base/entity/basic/search/SearchSmtMaterial.java
+++ ocean-common/ocean-common-base/ocean-imes-base/src/main/java/com/fantechs/common/base/entity/basic/search/SearchSmtMaterial.java
@@ -35,4 +35,16 @@ public class SearchSmtMaterial extends BaseQuery implements Serializable {
      */
     @ApiModelProperty(name="materialDesc" ,value="物料描述")
     private String materialDesc;
+
+    /**
+     * 产品BOM ID
+     */
+    @ApiModelProperty(name="productBomId" ,value="产品BOM ID")
+    private Long productBomId;
+
+    /**
+     * 工单ID
+     */
+    @ApiModelProperty(name="workOrderId" ,value="工单ID")
+    private Long workOrderId;
 }
