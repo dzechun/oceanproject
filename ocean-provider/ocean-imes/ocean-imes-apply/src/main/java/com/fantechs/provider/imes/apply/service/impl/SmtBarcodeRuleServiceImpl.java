94f707276e0cb1c2807a25b455b751e0fea2fe82
wangcaozhou <2049579718@qq.com>
2020-11-07 11:47:26 +0800

    条码规则功能

--- ocean-provider/ocean-imes/ocean-imes-apply/src/main/java/com/fantechs/provider/imes/apply/service/impl/SmtBarcodeRuleServiceImpl.java
+++ ocean-provider/ocean-imes/ocean-imes-apply/src/main/java/com/fantechs/provider/imes/apply/service/impl/SmtBarcodeRuleServiceImpl.java
@@ -23,8 +23,6 @@ import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  *
