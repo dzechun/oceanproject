004a5a1c610e4cf462ed08351fee432836a6ba40
wangcaozhou <2049579718@qq.com>
2020-11-09 14:54:18 +0800

    条码规则变更需求的初始化

--- ocean-provider/ocean-imes/ocean-imes-apply/src/main/java/com/fantechs/provider/imes/apply/controller/SmtBarcodeRuleSpecController.java
+++ ocean-provider/ocean-imes/ocean-imes-apply/src/main/java/com/fantechs/provider/imes/apply/controller/SmtBarcodeRuleSpecController.java
@@ -32,24 +32,6 @@ public class SmtBarcodeRuleSpecController {
     @Autowired
     private SmtBarcodeRuleSpecService smtBarcodeRuleSpecService;
 
-    @ApiOperation(value = "新增",notes = "新增")
-    @PostMapping("/add")
-    public ResponseEntity add(@ApiParam(value = "必传：",required = true)@RequestBody @Validated SmtBarcodeRuleSpec smtBarcodeRuleSpec) {
-        return ControllerUtil.returnCRUD(smtBarcodeRuleSpecService.save(smtBarcodeRuleSpec));
-    }
-
-    @ApiOperation("删除")
-    @PostMapping("/delete")
-    public ResponseEntity delete(@ApiParam(value = "对象ID列表，多个逗号分隔",required = true) @RequestParam @NotBlank(message="ids不能为空") String ids) {
-        return ControllerUtil.returnCRUD(smtBarcodeRuleSpecService.batchDelete(ids));
-    }
-
-    @ApiOperation("修改")
-    @PostMapping("/update")
-    public ResponseEntity update(@ApiParam(value = "对象，Id必传",required = true)@RequestBody @Validated(value=SmtBarcodeRuleSpec.update.class) SmtBarcodeRuleSpec smtBarcodeRuleSpec) {
-        return ControllerUtil.returnCRUD(smtBarcodeRuleSpecService.update(smtBarcodeRuleSpec));
-    }
-
     @ApiOperation(value = "批量新增",notes = "批量新增")
     @PostMapping("/batchSave")
     public ResponseEntity batchSave(@ApiParam(value = "必传：",required = true)@RequestBody List<SmtBarcodeRuleSpec> smtBarcodeRuleSpecs) {
@@ -63,6 +45,13 @@ public class SmtBarcodeRuleSpecController {
                                           @Validated(value=SmtBarcodeRuleSpec.update.class) List<SmtBarcodeRuleSpec> smtBarcodeRuleSpecs) {
         return ControllerUtil.returnCRUD(smtBarcodeRuleSpecService.batchUpdate(smtBarcodeRuleSpecs));
     }
+
+    @ApiOperation("删除")
+    @PostMapping("/delete")
+    public ResponseEntity delete(@ApiParam(value = "对象ID列表，多个逗号分隔",required = true) @RequestParam @NotBlank(message="ids不能为空") String ids) {
+        return ControllerUtil.returnCRUD(smtBarcodeRuleSpecService.batchDelete(ids));
+    }
+
 
     @ApiOperation("获取详情")
     @PostMapping("/detail")
