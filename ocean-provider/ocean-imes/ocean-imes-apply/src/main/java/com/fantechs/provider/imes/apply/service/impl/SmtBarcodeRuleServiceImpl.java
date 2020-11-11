2f3f25f11380cc3f4391d9be08921f1af2d2d948
wangcaozhou <2049579718@qq.com>
2020-11-11 14:32:19 +0800

    条码规则修复

--- ocean-provider/ocean-imes/ocean-imes-apply/src/main/java/com/fantechs/provider/imes/apply/service/impl/SmtBarcodeRuleServiceImpl.java
+++ ocean-provider/ocean-imes/ocean-imes-apply/src/main/java/com/fantechs/provider/imes/apply/service/impl/SmtBarcodeRuleServiceImpl.java
@@ -156,7 +156,7 @@ public class SmtBarcodeRuleServiceImpl extends BaseService<SmtBarcodeRule> imple
         Long barcodeRuleId = smtBarcodeRule.getBarcodeRuleId();
         if(StringUtils.isEmpty(barcodeRuleId)){
             //新增条码规则
-            i = this.save(smtBarcodeRule);
+            i = smtBarcodeRuleMapper.insert(smtBarcodeRule);
 
             /**
              * 保存条码规则配置
