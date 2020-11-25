5b1a7850f3386d6b96c65ea0de6c4a99d887ff15
wangcaozhou <2049579718@qq.com>
2020-11-25 17:24:28 +0800

    工单流转卡任务池和工单条码任务池的查询

--- ocean-provider/ocean-imes/ocean-imes-apply/src/main/java/com/fantechs/provider/imes/apply/controller/SmtWorkOrderCardPoolController.java
+++ ocean-provider/ocean-imes/ocean-imes-apply/src/main/java/com/fantechs/provider/imes/apply/controller/SmtWorkOrderCardPoolController.java
@@ -33,24 +33,6 @@ public class SmtWorkOrderCardPoolController {
     @Autowired
     private SmtWorkOrderCardPoolService smtWorkOrderCardPoolService;
 
-    @ApiOperation(value = "新增",notes = "新增")
-    @PostMapping("/add")
-    public ResponseEntity add(@ApiParam(value = "必传：",required = true)@RequestBody  SmtWorkOrderCardPool smtWorkOrderCardPool) {
-        return ControllerUtil.returnCRUD(smtWorkOrderCardPoolService.save(smtWorkOrderCardPool));
-    }
-
-    @ApiOperation("删除")
-    @PostMapping("/delete")
-    public ResponseEntity delete(@ApiParam(value = "对象ID列表，多个逗号分隔",required = true) @RequestParam @NotBlank(message="ids不能为空") String ids) {
-        return ControllerUtil.returnCRUD(smtWorkOrderCardPoolService.batchDelete(ids));
-    }
-
-    @ApiOperation("修改")
-    @PostMapping("/update")
-    public ResponseEntity update(@ApiParam(value = "对象，Id必传",required = true)@RequestBody SmtWorkOrderCardPool smtWorkOrderCardPool) {
-        return ControllerUtil.returnCRUD(smtWorkOrderCardPoolService.update(smtWorkOrderCardPool));
-    }
-
     @ApiOperation("获取详情")
     @PostMapping("/detail")
     public ResponseEntity<SmtWorkOrderCardPool> detail(@ApiParam(value = "ID",required = true)@RequestParam  @NotNull(message="id不能为空") Long id) {
