cb050651089357a1097be3a23f1b29941e672593
‘jiangbinbin <idealLife2020@163.com>
2020-12-21 16:04:25 +0800

    完善质检计划跟质检单的实体

---
+++ ocean-provider/ocean-qms/src/main/java/com/fantechs/provider/qms/OceanQmsApplication.java
@@ -0,0 +1,19 @@
+package com.fantechs.provider.qms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan({"com.fantechs.provider.qms","com.fantechs.common"})
+@MapperScan({"com.fantechs.provider.qms.mapper"})
+public class OceanQmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanQmsApplication.class, args);
+    }
+
+}
