f6539c7ecb790d8307f0432b05c04e2ae21be5cb
leifengzhi <ll123456>
2021-01-12 13:51:46 +0800

    bug

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/security/SysCustomForm.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/security/SysCustomForm.java
@@ -53,7 +53,6 @@ public class SysCustomForm  extends ValidGroup implements  Serializable {
      */
     @ApiModelProperty(name="subId",value = "关联表单")
     @Excel(name = "关联表单", height = 20, width = 30,orderNum="")
-    @Id
     @Column(name = "sub_id")
     private Long subId;
 
