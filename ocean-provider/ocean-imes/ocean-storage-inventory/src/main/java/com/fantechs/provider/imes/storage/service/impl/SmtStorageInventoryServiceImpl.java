28d0e157b6caf83617127faec4a4bf45c811bea8
‘jiangbinbin <idealLife2020@163.com>
2021-01-13 21:17:26 +0800

    修改储位库存增加逻辑

--- ocean-provider/ocean-imes/ocean-storage-inventory/src/main/java/com/fantechs/provider/imes/storage/service/impl/SmtStorageInventoryServiceImpl.java
+++ ocean-provider/ocean-imes/ocean-storage-inventory/src/main/java/com/fantechs/provider/imes/storage/service/impl/SmtStorageInventoryServiceImpl.java
@@ -2,17 +2,21 @@ package com.fantechs.provider.imes.storage.service.impl;
 
 import com.fantechs.common.base.constants.ErrorCodeEnum;
 import com.fantechs.common.base.dto.storage.SmtStorageInventoryDto;
+import com.fantechs.common.base.entity.basic.search.SearchSmtStorageInventory;
 import com.fantechs.common.base.entity.security.SysUser;
 import com.fantechs.common.base.entity.storage.SmtStorageInventory;
 import com.fantechs.common.base.exception.BizErrorException;
+import com.fantechs.common.base.response.ResponseEntity;
 import com.fantechs.common.base.support.BaseService;
 import com.fantechs.common.base.utils.CurrentUserInfoUtils;
 import com.fantechs.common.base.utils.StringUtils;
 import com.fantechs.provider.imes.storage.mapper.SmtStorageInventoryMapper;
 import com.fantechs.provider.imes.storage.service.SmtStorageInventoryService;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -39,13 +43,31 @@ public class SmtStorageInventoryServiceImpl  extends BaseService<SmtStorageInven
             throw new BizErrorException(ErrorCodeEnum.UAC10011039);
         }
 
-        storageInventory.setCreateTime(new Date());
-        storageInventory.setCreateUserId(user.getUserId());
-        storageInventory.setModifiedTime(new Date());
-        storageInventory.setModifiedUserId(user.getUserId());
-        storageInventory.setStatus(StringUtils.isEmpty(storageInventory.getStatus())?1:storageInventory.getStatus());
+        Example example =new Example(SmtStorageInventory.class);
+        example.createCriteria().andEqualTo("storageId",storageInventory.getStorageId())
+                .andEqualTo("materialId",storageInventory.getMaterialId());
 
-        int i = smtStorageInventoryMapper.insertUseGeneratedKeys(storageInventory);
+        List<SmtStorageInventory> smtStorageInventories = smtStorageInventoryMapper.selectByExample(example);
+
+        int i = 0;
+        if(StringUtils.isNotEmpty(smtStorageInventories)){
+            BigDecimal quantity = storageInventory.getQuantity();
+            storageInventory = smtStorageInventories.get(0);
+            //累加库存
+            quantity = BigDecimal.valueOf(storageInventory.getQuantity().intValue()+quantity.intValue());
+            storageInventory.setQuantity(quantity);
+            super.update(storageInventory);
+        } else {
+            //新增库存
+            storageInventory.setCreateTime(new Date());
+            storageInventory.setCreateUserId(user.getUserId());
+            storageInventory.setModifiedTime(new Date());
+            storageInventory.setModifiedUserId(user.getUserId());
+            storageInventory.setStatus(StringUtils.isEmpty(storageInventory.getStatus())?1:storageInventory.getStatus());
+            storageInventory.setOrganizationId(user.getOrganizationId());
+
+            i = smtStorageInventoryMapper.insertUseGeneratedKeys(storageInventory);
+        }
 
         return i;
     }
