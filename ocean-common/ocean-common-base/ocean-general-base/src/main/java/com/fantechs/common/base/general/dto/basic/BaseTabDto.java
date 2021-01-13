a0e6e1a63cd429c7fd99ce93bfedb3882a16a45d
wangxiaolin <wangxiaolin@qq.com>
2021-01-13 10:29:24 +0800

    页签管理

---
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/basic/BaseTabDto.java
@@ -0,0 +1,10 @@
+package com.fantechs.common.base.general.dto.basic;
+
+import com.fantechs.common.base.general.entity.basic.BaseTab;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class BaseTabDto extends BaseTab implements Serializable {
+}
