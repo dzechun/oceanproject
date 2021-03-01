121803fad4b2fcf88e6e1e136f036aafac08a6f9
wangxiaolin <wangxiaolin@qq.com>
2021-03-01 19:19:53 +0800

    1、批量导入 2、包装规格bug修复

---
+++ ocean-common/ocean-common-base/ocean-imes-base/src/main/java/com/fantechs/common/base/dto/basic/imports/SmtFactoryImport.java
@@ -0,0 +1,60 @@
+package com.fantechs.common.base.dto.basic.imports;
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+@Data
+public class SmtFactoryImport implements Serializable {
+
+    /**
+     * 工厂编码
+     */
+    @Excel(name = "工厂编码(必填)",  height = 20, width = 30)
+    @ApiModelProperty(name="factoryCode" ,value="厂别编码")
+    private String factoryCode;
+
+    /**
+     * 工厂名称
+     */
+    @Excel(name = "工厂名称(必填)", height = 20, width = 30)
+    @ApiModelProperty(name="factoryName" ,value="厂别名称")
+    private String factoryName;
+
+    /**
+     * 工厂描述
+     */
+    @Excel(name = "工厂描述", height = 20, width = 30)
+    @ApiModelProperty(name="factoryDesc" ,value="厂别描述")
+    private String factoryDesc;
+
+    /**
+     * 组织id
+     */
+    @ApiModelProperty(name="organizationId",value = "组织id")
+    private Long organizationId;
+
+    /**
+     * 组织编码
+     */
+    @ApiModelProperty(name = "organizationCode",value = "组织编码")
+    @Excel(name = "组织编码", height = 20, width = 30)
+    private String organizationCode;
+
+    /**
+     * 备注
+     */
+    @ApiModelProperty(name="remark",value = "备注")
+    @Excel(name = "备注", height = 20, width = 30)
+    private String remark;
+
+    /**
+     * 工厂状态（0、不启用 1、启用）
+     */
+    @ApiModelProperty(name="status" ,value="工厂状态（0、不启用 1、启用）")
+    @Excel(name = "工厂状态（0、不启用 1、启用）", height = 20, width = 30)
+    private Integer status;
+}
