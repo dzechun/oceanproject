dde99387bfb1374d289039f66bd65ddabfa552dd
‘jiangbinbin <idealLife2020@163.com>
2021-03-09 10:00:41 +0800

    员工工作量报表查询

---
+++ ocean-ureport/src/main/java/com/fantechs/mapper/EmpWorkloadMapper.java
@@ -0,0 +1,16 @@
+package com.fantechs.mapper;
+
+import com.fantechs.common.base.mybatis.MyMapper;
+import com.fantechs.dto.EmpWorkload;
+import com.fantechs.entity.search.SearchEmpWorkload;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface EmpWorkloadMapper extends MyMapper<EmpWorkload> {
+
+    List<Map<String, Object>> findHistogram(SearchEmpWorkload searchEmpWorkload);
+
+}
