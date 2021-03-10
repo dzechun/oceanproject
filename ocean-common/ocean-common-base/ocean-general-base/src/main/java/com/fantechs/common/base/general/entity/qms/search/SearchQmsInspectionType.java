1f75171c59aac856285c4d78219999f4650568f2
‘jiangbinbin <idealLife2020@163.com>
2021-03-10 13:59:52 +0800

    提交检验项目、类型实体

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/qms/search/SearchQmsInspectionType.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/qms/search/SearchQmsInspectionType.java
@@ -1,23 +1,17 @@
 package com.fantechs.common.base.general.entity.qms.search;
 
-import cn.afterturn.easypoi.excel.annotation.Excel;
-import com.alibaba.fastjson.annotation.JSONField;
 import com.fantechs.common.base.dto.BaseQuery;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.util.Date;
 
 ;
 
 /**
  * 检验类型表
  * qms_inspection_type
+ *
  * @author jbb
  * @date 2020-12-23 11:09:14
  */
@@ -27,26 +21,32 @@ public class SearchQmsInspectionType extends BaseQuery implements Serializable {
     /**
      * 检验类型单号
      */
-    @ApiModelProperty(name="inspectionTypeCode",value = "检验类型单号")
+    @ApiModelProperty(name = "inspectionTypeCode", value = "检验类型单号")
     private String inspectionTypeCode;
 
     /**
      * 检验类型名称
      */
-    @ApiModelProperty(name="inspectionTypeName",value = "检验类型名称")
+    @ApiModelProperty(name = "inspectionTypeName", value = "检验类型名称")
     private String inspectionTypeName;
 
     /**
      * 检验类型水平
      */
-    @ApiModelProperty(name="inspectionTypeLevel",value = "检验类型水平")
+    @ApiModelProperty(name = "inspectionTypeLevel", value = "检验类型水平")
     private Long inspectionTypeLevel;
 
     /**
      * 检验项
      */
-    @ApiModelProperty(name="inspectionNape",value = "检验项")
+    @ApiModelProperty(name = "inspectionNape", value = "检验项")
     private Long inspectionNape;
+
+    /**
+     * 根据编码查询方式标记（传1则为等值查询）
+     */
+    @ApiModelProperty(name = "queryMark", value = "查询方式标记")
+    private Byte codeQueryMark;
 
 
     private static final long serialVersionUID = 1L;
