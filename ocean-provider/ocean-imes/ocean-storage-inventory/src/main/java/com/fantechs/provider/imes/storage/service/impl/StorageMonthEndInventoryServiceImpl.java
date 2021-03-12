1b87c527bd832659d9c073ec88dd26802a1a3307
‘jiangbinbin <idealLife2020@163.com>
2021-03-12 17:57:55 +0800

    月末库存报表

--- ocean-provider/ocean-imes/ocean-storage-inventory/src/main/java/com/fantechs/provider/imes/storage/service/impl/StorageMonthEndInventoryServiceImpl.java
+++ ocean-provider/ocean-imes/ocean-storage-inventory/src/main/java/com/fantechs/provider/imes/storage/service/impl/StorageMonthEndInventoryServiceImpl.java
@@ -1,17 +1,8 @@
 package com.fantechs.provider.imes.storage.service.impl;
 
-import com.fantechs.common.base.constants.ErrorCodeEnum;
-import com.fantechs.common.base.dto.storage.SmtStorageInventoryDto;
 import com.fantechs.common.base.dto.storage.StorageMonthEndInventoryDto;
-import com.fantechs.common.base.entity.basic.SmtMaterial;
-import com.fantechs.common.base.entity.basic.search.SearchSmtMaterial;
-import com.fantechs.common.base.entity.security.SysUser;
-import com.fantechs.common.base.entity.storage.SmtStorageInventoryDet;
 import com.fantechs.common.base.entity.storage.StorageMonthEndInventory;
-import com.fantechs.common.base.exception.BizErrorException;
 import com.fantechs.common.base.support.BaseService;
-import com.fantechs.common.base.utils.CurrentUserInfoUtils;
-import com.fantechs.common.base.utils.StringUtils;
 import com.fantechs.provider.api.imes.basic.BasicFeignApi;
 import com.fantechs.provider.imes.storage.mapper.SmtStorageInventoryMapper;
 import com.fantechs.provider.imes.storage.mapper.StorageMonthEndInventoryMapper;
@@ -19,8 +10,10 @@ import com.fantechs.provider.imes.storage.service.StorageMonthEndInventoryServic
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class StorageMonthEndInventoryServiceImpl extends BaseService<StorageMonthEndInventory> implements StorageMonthEndInventoryService {
@@ -38,53 +31,63 @@ public class StorageMonthEndInventoryServiceImpl extends BaseService<StorageMont
     }
 
     @Override
+    public List<StorageMonthEndInventoryDto> findMonthEndList(Map<String, Object> map) {
+        return storageMonthEndInventoryMapper.findMonthEndList(map);
+    }
+
+    @Override
     public int record() {
-        SysUser user = CurrentUserInfoUtils.getCurrentUserInfo();
-        if (StringUtils.isEmpty(user)) {
-            throw new BizErrorException(ErrorCodeEnum.UAC10011039);
-        }
+//        SysUser user = CurrentUserInfoUtils.getCurrentUserInfo();
+//        if (StringUtils.isEmpty(user)) {
+//            throw new BizErrorException(ErrorCodeEnum.UAC10011039);
+//        }
         Map<String, Object> map = new HashMap<>();
-        List<StorageMonthEndInventory> storageMonthEndInventorys = new ArrayList<>();
-        List<SmtStorageInventoryDto> list = smtStorageInventoryMapper.findList(map);
-        for (SmtStorageInventoryDto smtStorageInventoryDto : list) {
-            if (StringUtils.isNotEmpty(smtStorageInventoryDto.getSmtStorageInventoryDets())) {
-                for (SmtStorageInventoryDet smtStorageInventoryDet : smtStorageInventoryDto.getSmtStorageInventoryDets()) {
-                    StorageMonthEndInventory storageMonthEndInventory = new StorageMonthEndInventory();
-                    storageMonthEndInventory.setStorageId(smtStorageInventoryDto.getStorageId());
-                    storageMonthEndInventory.setMaterialId(smtStorageInventoryDto.getMaterialId());
-                    storageMonthEndInventory.setTotal(smtStorageInventoryDet.getMaterialQuantity().intValue() <= 0 ? 0 : smtStorageInventoryDet.getMaterialQuantity().intValue());
-                    storageMonthEndInventory.setMaterialBarcodeCode(smtStorageInventoryDet.getMaterialBarcodeCode());
-                    storageMonthEndInventory.setCreateTime(new Date());
-                    storageMonthEndInventory.setModifiedTime(new Date());
-
-                    storageMonthEndInventory.setCreateUserId(user.getUserId());
-                    storageMonthEndInventory.setModifiedUserId(user.getUserId());
-                    storageMonthEndInventory.setOrganizationId(user.getOrganizationId());
-
-                    SearchSmtMaterial searchSmtMaterial = new SearchSmtMaterial();
-                    searchSmtMaterial.setMaterialId(smtStorageInventoryDto.getMaterialId());
-                    List<SmtMaterial> smtMaterialList = basicFeignApi.findSmtMaterialList(searchSmtMaterial).getData();
-
-                    if (StringUtils.isNotEmpty(smtMaterialList) && smtStorageInventoryDto.getQuantity().intValue() > 0) {
-                        if (smtMaterialList.get(0).getBaseTabDto() != null) {
-                            BigDecimal number = smtMaterialList.get(0).getBaseTabDto().getPackageSpecificationQuantity();
-                            if (StringUtils.isNotEmpty(number)) {
-                                Double ceil = Math.ceil(smtStorageInventoryDto.getQuantity().intValue() / number.intValue());
-                                storageMonthEndInventory.setBoxNumber(ceil.intValue());
-                            } else {
-                                storageMonthEndInventory.setBoxNumber(smtStorageInventoryDto.getQuantity().intValue());
-                            }
-                        } else {
-                            storageMonthEndInventory.setBoxNumber(smtStorageInventoryDto.getQuantity().intValue());
-                        }
-                    } else {
-                        storageMonthEndInventory.setBoxNumber(0);
-                    }
-                    storageMonthEndInventorys.add(storageMonthEndInventory);
-                }
-            }
+//        List<StorageMonthEndInventory> storageMonthEndInventorys = new ArrayList<>();
+//        List<SmtStorageInventoryDto> list = smtStorageInventoryMapper.findList(map);
+        List<StorageMonthEndInventoryDto> list = storageMonthEndInventoryMapper.findList(map);
+        for (StorageMonthEndInventoryDto storageMonthEndInventoryDto : list) {
+            storageMonthEndInventoryDto.setCreateTime(new Date());
         }
+//        for (SmtStorageInventoryDto smtStorageInventoryDto : list) {
+//            if (StringUtils.isNotEmpty(smtStorageInventoryDto.getSmtStorageInventoryDets())) {
+//                for (SmtStorageInventoryDet smtStorageInventoryDet : smtStorageInventoryDto.getSmtStorageInventoryDets()) {
+//                    StorageMonthEndInventory storageMonthEndInventory = new StorageMonthEndInventory();
+//                    storageMonthEndInventory.setStorageId(smtStorageInventoryDto.getStorageId());
+//                    storageMonthEndInventory.setMaterialId(smtStorageInventoryDto.getMaterialId());
+//                    storageMonthEndInventory.setTotal(smtStorageInventoryDet.getMaterialQuantity().intValue() <= 0 ? 0 : smtStorageInventoryDet.getMaterialQuantity().intValue());
+//                    storageMonthEndInventory.setMaterialBarcodeCode(smtStorageInventoryDet.getMaterialBarcodeCode());
+//                    storageMonthEndInventory.setMaterialTotal(smtStorageInventoryDto.getQuantity() == null ? 0 : smtStorageInventoryDto.getQuantity().intValue());
+//                    storageMonthEndInventory.setCreateTime(new Date());
+//                    storageMonthEndInventory.setModifiedTime(new Date());
+//
+//                    storageMonthEndInventory.setCreateUserId(user.getUserId());
+//                    storageMonthEndInventory.setModifiedUserId(user.getUserId());
+//                    storageMonthEndInventory.setOrganizationId(user.getOrganizationId());
+//
+//                    SearchSmtMaterial searchSmtMaterial = new SearchSmtMaterial();
+//                    searchSmtMaterial.setMaterialId(smtStorageInventoryDto.getMaterialId());
+//                    List<SmtMaterial> smtMaterialList = basicFeignApi.findSmtMaterialList(searchSmtMaterial).getData();
+//
+//                    if (StringUtils.isNotEmpty(smtMaterialList) && smtStorageInventoryDto.getQuantity().intValue() > 0) {
+//                        if (smtMaterialList.get(0).getBaseTabDto() != null) {
+//                            BigDecimal number = smtMaterialList.get(0).getBaseTabDto().getPackageSpecificationQuantity();
+//                            if (StringUtils.isNotEmpty(number)) {
+//                                Double ceil = Math.ceil(smtStorageInventoryDto.getQuantity().intValue() / number.intValue());
+//                                storageMonthEndInventory.setBoxNumber(ceil.intValue());
+//                            } else {
+//                                storageMonthEndInventory.setBoxNumber(smtStorageInventoryDto.getQuantity().intValue());
+//                            }
+//                        } else {
+//                            storageMonthEndInventory.setBoxNumber(smtStorageInventoryDto.getQuantity().intValue());
+//                        }
+//                    } else {
+//                        storageMonthEndInventory.setBoxNumber(0);
+//                    }
+//                    storageMonthEndInventorys.add(storageMonthEndInventory);
+//                }
+//            }
+//        }
 
-        return storageMonthEndInventoryMapper.insertList(storageMonthEndInventorys);
+        return storageMonthEndInventoryMapper.insertList(list);
     }
 }
