12f063032303b35bcba770dd8fb799ff23ab8f76
leifengzhi <ll123456>
2021-03-12 20:06:51 +0800

    拆分日志

---
+++ ocean-provider/ocean-log/src/main/java/com/fantechs/log/OceanLogApplication.java
@@ -0,0 +1,13 @@
+package com.fantechs.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanLogApplication.class, args);
+    }
+
+}
