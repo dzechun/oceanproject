4b6b1f0d58d991e095bf914c71c3aef5af1f1074
‘jiangbinbin <idealLife2020@163.com>
2021-03-24 09:52:12 +0800

    解决供应商绑定地址问题

--- ocean-provider/ocean-imes/ocean-imes-basic/src/main/java/com/fantechs/provider/imes/basic/mapper/SmtSupplierAddressMapper.java
+++ ocean-provider/ocean-imes/ocean-imes-basic/src/main/java/com/fantechs/provider/imes/basic/mapper/SmtSupplierAddressMapper.java
@@ -3,7 +3,6 @@ package com.fantechs.provider.imes.basic.mapper;
 import com.fantechs.common.base.entity.basic.SmtSupplierAddress;
 import com.fantechs.common.base.mybatis.MyMapper;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
@@ -11,6 +10,4 @@ import java.util.Map;
 @Mapper
 public interface SmtSupplierAddressMapper extends MyMapper<SmtSupplierAddress> {
     List<SmtSupplierAddress> findList(Map<String, Object> map);
-
-    int updateIfDefault(@Param("addressId") Long addressId,@Param("supplierId") Long supplierId);
 }
