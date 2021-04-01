1efa7560f48103e07a3cce7ca70a3e1d74d6235e
huangyicheng <huangyicheng123456>
2021-04-01 10:15:16 +0800

    test master

--- ocean-provider/ocean-wms/ocean-wms-out/src/main/java/com/fantechs/provider/wms/out/service/impl/WmsOutShippingNoteServiceImpl.java
+++ ocean-provider/ocean-wms/ocean-wms-out/src/main/java/com/fantechs/provider/wms/out/service/impl/WmsOutShippingNoteServiceImpl.java
@@ -256,7 +256,6 @@ public class WmsOutShippingNoteServiceImpl extends BaseService<WmsOutShippingNot
 
         int result = wmsOutShippingNoteMapper.insertUseGeneratedKeys(wmsOutShippingNote);
 
-        System.out.println("456");
         //履历
         WmsOutHtShippingNote wmsOutHtShippingNote = new WmsOutHtShippingNote();
         BeanUtils.copyProperties(wmsOutShippingNote, wmsOutHtShippingNote);
