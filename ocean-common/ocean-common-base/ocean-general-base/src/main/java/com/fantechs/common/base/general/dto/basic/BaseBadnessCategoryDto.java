e10c17d837b5b70b0a7840d679f2ef25a69a5483
‘jiangbinbin <idealLife2020@163.com>
2021-04-02 17:40:34 +0800

    完成不良类别代码

---
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/basic/BaseBadnessCategoryDto.java
@@ -0,0 +1,44 @@
+package com.fantechs.common.base.general.dto.basic;
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fantechs.common.base.general.entity.basic.BaseBadnessCategory;
+import com.fantechs.common.base.support.ValidGroup;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Data
+public class BaseBadnessCategoryDto extends BaseBadnessCategory implements Serializable {
+
+    /**
+     * 创建用户名称
+     */
+    @Transient
+    @ApiModelProperty(name = "createUserName",value = "创建用户名称")
+    @Excel(name = "创建用户名称", height = 20, width = 30,orderNum="6")
+    private String createUserName;
+
+    /**
+     * 修改用户名称
+     */
+    @Transient
+    @ApiModelProperty(name = "createUserName",value = "修改用户名称")
+    @Excel(name = "修改用户名称", height = 20, width = 30,orderNum="8")
+    private String modifiedUserName;
+
+    /**
+     * 组织名称
+     */
+    @Transient
+    @ApiModelProperty(name = "organizationName",value = "组织名称")
+    private String organizationName;
+
+}
