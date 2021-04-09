d1ddcec90d0d7e2ca9520ce94a91e8a70218fe67
wangxiaolin <wangxiaolin@qq.com>
2021-04-09 15:33:31 +0800

    优化

--- ocean-provider/ocean-imes/ocean-imes-basic/src/main/java/com/fantechs/provider/imes/basic/controller/SmtDeptController.java
+++ ocean-provider/ocean-imes/ocean-imes-basic/src/main/java/com/fantechs/provider/imes/basic/controller/SmtDeptController.java
@@ -120,7 +120,7 @@ public class SmtDeptController {
      * @throws
      */
     @PostMapping(value = "/import")
-    @ApiOperation(value = "从excel导入电子标签信息",notes = "从excel导入电子标签信息")
+    @ApiOperation(value = "从excel导入部门信息",notes = "从excel导入部门信息")
     public ResponseEntity importExcel(@ApiParam(value ="输入excel文件",required = true)
                                       @RequestPart(value="file") MultipartFile file){
         try {
