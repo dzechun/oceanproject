55ac6fa9aee2f08f1bec2c071be623b18ae55e35
雷秦林 <lql@fantechs.com.cn>
2021-04-12 15:26:53 +0800

    mes-sfc配置

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/basic/history/BaseHtPackageSpecification.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/basic/history/BaseHtPackageSpecification.java
@@ -2,7 +2,7 @@ package com.fantechs.common.base.general.entity.basic.history;
 
 import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.alibaba.fastjson.annotation.JSONField;;
-import com.fantechs.common.base.dto.basic.BaseMaterialPackageDto;
+import com.fantechs.common.base.general.dto.basic.BaseMaterialPackageDto;
 import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
 import java.math.BigDecimal;
