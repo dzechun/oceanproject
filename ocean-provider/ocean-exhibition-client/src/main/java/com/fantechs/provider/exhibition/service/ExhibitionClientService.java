87b4965cd0aaa232032ebe29b8d75ea63024f12b
wangxiaolin <wangxiaolin@qq.com>
2021-04-16 11:07:18 +0800

    删除无用代码

--- ocean-provider/ocean-exhibition-client/src/main/java/com/fantechs/provider/exhibition/service/ExhibitionClientService.java
+++
@@ -1,8 +0,0 @@
-package com.fantechs.provider.exhibition.service;
-
-public interface ExhibitionClientService {
-    int makingOrders(Long orderId) throws Exception;
-    String agvStockTask(Long stockId, Integer type) throws Exception;
-    String agvStockTaskTest(String materialCode) throws Exception;
-    String agvContinueTask();
-}
