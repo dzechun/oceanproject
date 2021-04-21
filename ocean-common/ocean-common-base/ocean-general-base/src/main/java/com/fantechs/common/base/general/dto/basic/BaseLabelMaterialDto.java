b5ce48acd137add3fce8ed9eda11f456793c4b98
wangxiaolin <wangxiaolin@qq.com>
2021-04-21 09:49:35 +0800

    标签查询bug修复

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/basic/BaseLabelMaterialDto.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/basic/BaseLabelMaterialDto.java
@@ -36,39 +36,46 @@ public class BaseLabelMaterialDto extends BaseLabelMaterial implements Serializa
     @Excel(name = "产品版本",height = 20,width = 30,orderNum = "3")
     private String materialDesc;
     /**
+     * 标签编码
+     */
+    @Transient
+    @ApiModelProperty(name = "labelCode",value = "标签编码")
+    @Excel(name = "标签编码",height = 20,width = 30,orderNum = "4")
+    private String labelCode;
+    /**
      * 标签名称
      */
     @Transient
     @ApiModelProperty(name = "labelName",value = "标签名称")
-    @Excel(name = "标签名称",height = 20,width = 30,orderNum = "4")
+    @Excel(name = "标签名称",height = 20,width = 30,orderNum = "5")
     private String labelName;
     /**
      * 标签版本
      */
     @Transient
     @ApiModelProperty(name = "labelVersion",value = "标签版本")
-    @Excel(name = "标签版本",height = 20,width = 30,orderNum = "5")
+    @Excel(name = "标签版本",height = 20,width = 30,orderNum = "6")
     private String labelVersion;
     /**
      * 标签类别
      */
     @Transient
     @ApiModelProperty(name = "labelCategoryName",value = "标签类别")
-    @Excel(name = "标签类别",height = 20,width = 30,orderNum = "6")
+    @Excel(name = "标签类别",height = 20,width = 30,orderNum = "7")
     private String labelCategoryName;
     /**
      * 工序名称
      */
     @Transient
     @ApiModelProperty(name = "processName",value = "工序名称")
-    @Excel(name = "工序名称",height = 20,width = 30,orderNum = "7")
+    @Excel(name = "工序名称",height = 20,width = 30,orderNum = "8")
     private String processName;
     /**
      * 创建用户名称
      */
     @Transient
     @ApiModelProperty(name = "createUserName",value = "创建用户名称")
-    @Excel(name = "创建用户名称", height = 20, width = 30,orderNum="8")
+    @Excel(name = "创建用户名称", height = 20, width = 30,orderNum="9")
     private String createUserName;
 
     /**
@@ -76,7 +83,7 @@ public class BaseLabelMaterialDto extends BaseLabelMaterial implements Serializa
      */
     @Transient
     @ApiModelProperty(name = "createUserName",value = "修改用户名称")
-    @Excel(name = "修改用户名称", height = 20, width = 30,orderNum="10")
+    @Excel(name = "修改用户名称", height = 20, width = 30,orderNum="11")
     private String modifiedUserName;
 
     /**
