727f4c004aa9565a17ca1704e5d82570f7ccfd83
‘jiangbinbin <idealLife2020@163.com>
2021-04-25 11:30:15 +0800

    开发物料专用库位、工作区

---
+++ ocean-provider/ocean-base/src/main/java/com/fantechs/provider/base/controller/BaseWorkingAreaController.java
@@ -0,0 +1,93 @@
+package com.fantechs.provider.base.controller;
+
+import com.fantechs.common.base.exception.BizErrorException;
+import com.fantechs.common.base.general.dto.basic.BaseWorkingAreaDto;
+import com.fantechs.common.base.general.entity.basic.BaseWorkingArea;
+import com.fantechs.common.base.general.entity.basic.history.BaseHtWorkingArea;
+import com.fantechs.common.base.general.entity.basic.search.SearchBaseWorkingArea;
+import com.fantechs.common.base.response.ControllerUtil;
+import com.fantechs.common.base.response.ResponseEntity;
+import com.fantechs.common.base.utils.EasyPoiUtils;
+import com.fantechs.common.base.utils.StringUtils;
+import com.fantechs.provider.base.service.BaseHtWorkingAreaService;
+import com.fantechs.provider.base.service.BaseWorkingAreaService;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@RestController
+@Api(tags = "工作区信息")
+@RequestMapping("/baseWorkingArea")
+@Validated
+public class BaseWorkingAreaController {
+
+    @Resource
+    private BaseWorkingAreaService baseWorkingAreaService;
+
+    @Resource
+    private BaseHtWorkingAreaService baseHtWorkingAreaService;
+
+    @ApiOperation(value = "新增",notes = "新增")
+    @PostMapping("/add")
+    public ResponseEntity add(@ApiParam(value = "必传：",required = true)@RequestBody @Validated BaseWorkingArea baseWorkingArea) {
+        return ControllerUtil.returnCRUD(baseWorkingAreaService.save(baseWorkingArea));
+    }
+
+    @ApiOperation("删除")
+    @PostMapping("/delete")
+    public ResponseEntity delete(@ApiParam(value = "对象ID列表，多个逗号分隔",required = true) @RequestParam @NotBlank(message="ids不能为空") String ids) {
+        return ControllerUtil.returnCRUD(baseWorkingAreaService.batchDelete(ids));
+    }
+
+    @ApiOperation("修改")
+    @PostMapping("/update")
+    public ResponseEntity update(@ApiParam(value = "对象，Id必传",required = true)@RequestBody @Validated(value=BaseWorkingArea.update.class) BaseWorkingArea baseWorkingArea) {
+        return ControllerUtil.returnCRUD(baseWorkingAreaService.update(baseWorkingArea));
+    }
+
+    @ApiOperation("获取详情")
+    @PostMapping("/detail")
+    public ResponseEntity<BaseWorkingArea> detail(@ApiParam(value = "ID",required = true)@RequestParam  @NotNull(message="id不能为空") Long id) {
+        BaseWorkingArea  baseWorkingArea = baseWorkingAreaService.selectByKey(id);
+        return  ControllerUtil.returnDataSuccess(baseWorkingArea,StringUtils.isEmpty(baseWorkingArea)?0:1);
+    }
+
+    @ApiOperation("列表")
+    @PostMapping("/findList")
+    public ResponseEntity<List<BaseWorkingAreaDto>> findList(@ApiParam(value = "查询对象")@RequestBody SearchBaseWorkingArea searchBaseWorkingArea) {
+        Page<Object> page = PageHelper.startPage(searchBaseWorkingArea.getStartPage(),searchBaseWorkingArea.getPageSize());
+        List<BaseWorkingAreaDto> list = baseWorkingAreaService.findList(ControllerUtil.dynamicConditionByEntity(searchBaseWorkingArea));
+        return ControllerUtil.returnDataSuccess(list,(int)page.getTotal());
+    }
+
+    @ApiOperation("历史列表")
+    @PostMapping("/findHtList")
+    public ResponseEntity<List<BaseHtWorkingArea>> findHtList(@ApiParam(value = "查询对象")@RequestBody SearchBaseWorkingArea searchBaseWorkingArea) {
+        Page<Object> page = PageHelper.startPage(searchBaseWorkingArea.getStartPage(),searchBaseWorkingArea.getPageSize());
+        List<BaseHtWorkingArea> list = baseHtWorkingAreaService.findList(ControllerUtil.dynamicConditionByEntity(searchBaseWorkingArea));
+        return ControllerUtil.returnDataSuccess(list,(int)page.getTotal());
+    }
+
+    @PostMapping(value = "/export")
+    @ApiOperation(value = "导出excel",notes = "导出excel",produces = "application/octet-stream")
+    public void exportExcel(HttpServletResponse response, @ApiParam(value = "查询对象")
+    @RequestBody(required = false) SearchBaseWorkingArea searchBaseWorkingArea){
+    List<BaseWorkingAreaDto> list = baseWorkingAreaService.findList(ControllerUtil.dynamicConditionByEntity(searchBaseWorkingArea));
+    try {
+        // 导出操作
+        EasyPoiUtils.exportExcel(list, "工作区导出信息", "工作区信息", BaseWorkingAreaDto.class, "工作区.xls", response);
+        } catch (Exception e) {
+        throw new BizErrorException(e);
+        }
+    }
+}
