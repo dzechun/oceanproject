39e96a504d83adfd529d5841faf32cef635a41b3
liangzhongwu <m15521035747@163.com>
2021-05-11 09:58:03 +0800

    收货人信息查询

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/basic/search/SearchBaseConsignee.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/basic/search/SearchBaseConsignee.java
@@ -14,12 +14,6 @@ public class SearchBaseConsignee extends BaseQuery implements Serializable {
     private static final long serialVersionUID = 4724079505651924663L;
 
     /**
-     * 收货人信息ID
-     */
-    @ApiModelProperty(name="consigneeId" ,value="收货人信息ID")
-    private Long consigneeId;
-
-    /**
      * 收货人编码
      */
     @ApiModelProperty(name="consigneeCode" ,value="收货人编码")
@@ -31,5 +25,22 @@ public class SearchBaseConsignee extends BaseQuery implements Serializable {
     @ApiModelProperty(name="consigneeName" ,value="收货人名称")
     private String consigneeName;
 
+    /**
+     * 公司名称
+     */
+    @ApiModelProperty(name="companyName" ,value="公司名称")
+    private String companyName;
+
+    /**
+     * 联系人名称
+     */
+    @ApiModelProperty(name="linkManName" ,value="联系人名称")
+    private String linkManName;
+
+    /**
+     * 货主名称
+     */
+    @ApiModelProperty(name="materialOwnerName" ,value="货主名称")
+    private String materialOwnerName;
 
 }
