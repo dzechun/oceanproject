bcfd9a9d5c07c8bb5d44042afde9d348fe073161
dengxinkun <dengxinkun@qq.com>
2021-05-19 16:37:48 +0800

    补打条码相关接口（表没确定，先给出相关接口、参数以及返回值）

--- ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/service/MesSfcBarcodeReprintService.java
+++ ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/service/MesSfcBarcodeReprintService.java
@@ -3,12 +3,18 @@ package com.fantechs.provider.mes.sfc.service;
 public interface MesSfcBarcodeReprintService {
 
     /**
-     *
-     * @param keyword
-     * @param codeType
+     * 查找条码
+     * @param keyword 条码编号，模糊匹配
+     * @param codeType 1:包箱，2:栈板
      * @return
      */
     String findCode(String keyword, String codeType);
 
+    /**
+     * 重打条码
+     * @param barCode 条码编码
+     * @param codeType 1:包箱，2:栈板
+     * @return
+     */
     int reprintBarcode(String barCode, String codeType);
 }
--- ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/service/impl/MesSfcBarcodeReprintServiceImpl.java
+++ ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/service/impl/MesSfcBarcodeReprintServiceImpl.java
@@ -5,13 +5,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MesSfcBarcodeReprintServiceImpl implements MesSfcBarcodeReprintService {
+
     @Override
     public String findCode(String keyword, String codeType) {
-        return null;
+        String barCode = null;
+        if(codeType.equals("1")){
+
+        }else if(codeType.equals("2")){
+
+        }
+        return barCode;
     }
 
     @Override
     public int reprintBarcode(String barCode, String codeType) {
+        // 生成补打记录
+        // 调用打印机打印
         return 0;
     }
 }
