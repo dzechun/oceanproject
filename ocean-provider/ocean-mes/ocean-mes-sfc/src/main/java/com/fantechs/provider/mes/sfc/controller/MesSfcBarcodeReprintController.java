03511fdb26bbaeb5c60ebdddbca4ffe3899be772
dengxinkun <dengxinkun@qq.com>
2021-05-19 15:52:21 +0800

    产出工序不校验下道工序

---
+++ ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/controller/MesSfcBarcodeReprintController.java
@@ -0,0 +1,18 @@
+package com.fantechs.provider.mes.sfc.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 生产管理-包箱栈板条码补打控制器
+ *
+ * @author bgkun
+ * @date 2021-05-19
+ */
+@RestController
+@Api(tags = "生产管理-PDA包箱栈板条码补打作业")
+@RequestMapping("/mesSfcBarcodeReprint")
+public class MesSfcBarcodeReprintController {
+
+}
