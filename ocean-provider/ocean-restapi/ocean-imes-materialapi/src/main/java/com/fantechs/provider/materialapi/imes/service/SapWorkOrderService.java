5548801a83582291350685a4bffcfc4448ac837a
chipengfei <819474118@qq.com>
2021-05-25 17:59:32 +0800

    自动收货

--- ocean-provider/ocean-restapi/ocean-imes-materialapi/src/main/java/com/fantechs/provider/materialapi/imes/service/SapWorkOrderService.java
+++ ocean-provider/ocean-restapi/ocean-imes-materialapi/src/main/java/com/fantechs/provider/materialapi/imes/service/SapWorkOrderService.java
@@ -1,23 +1,15 @@
 package com.fantechs.provider.materialapi.imes.service;
 
 
-import com.fantechs.common.base.general.entity.basic.BaseMaterial;
 import com.fantechs.common.base.general.entity.mes.pm.search.SearchMesPmWorkOrder;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
-import java.util.List;
 
-@WebService(name = "MaterialService", // 暴露服务名称
+@WebService(name = "SapWorkOrderService", // 暴露服务名称
         targetNamespace = "http://imes.materialapi.provider.fantechs.com"// 命名空间,一般是接口的包名倒序
 )
-public interface MaterialService {
-
-    @WebMethod
-    String testMethod(String testName);
-
-    @WebMethod
-    String syncMaterial(List<BaseMaterial> baseMaterials);
+public interface SapWorkOrderService {
 
     @WebMethod
     String findWorkOrder(SearchMesPmWorkOrder searchMesPmWorkOrder);
--- ocean-provider/ocean-restapi/ocean-imes-materialapi/src/main/java/com/fantechs/provider/materialapi/imes/service/impl/SapWorkOrderServiceImpl.java
+++ ocean-provider/ocean-restapi/ocean-imes-materialapi/src/main/java/com/fantechs/provider/materialapi/imes/service/impl/SapWorkOrderServiceImpl.java
@@ -1,39 +1,24 @@
 package com.fantechs.provider.materialapi.imes.service.impl;
 
 import com.fantechs.common.base.general.dto.mes.pm.MesPmWorkOrderDto;
-import com.fantechs.common.base.general.entity.basic.BaseMaterial;
 import com.fantechs.common.base.general.entity.mes.pm.search.SearchMesPmWorkOrder;
 import com.fantechs.common.base.response.ResponseEntity;
 import com.fantechs.common.base.utils.StringUtils;
 import com.fantechs.provider.api.mes.pm.PMFeignApi;
-import com.fantechs.provider.materialapi.imes.service.MaterialService;
+import com.fantechs.provider.materialapi.imes.service.SapWorkOrderService;
 
 import javax.annotation.Resource;
 import javax.jws.WebService;
 import java.util.List;
 
-@WebService(serviceName = "MaterialService", // 与接口中指定的name一致
+@WebService(serviceName = "SapWorkOrderService", // 与接口中指定的name一致
         targetNamespace = "http://imes.materialapi.provider.fantechs.com", // 与接口中的命名空间一致,一般是接口的包名倒
-        endpointInterface = "com.fantechs.provider.materialapi.imes.service.MaterialService"// 接口地址
+        endpointInterface = "com.fantechs.provider.materialapi.imes.service.SapWorkOrderService"// 接口地址
 )
-public class MaterialServiceImpl implements MaterialService {
+public class SapWorkOrderServiceImpl implements SapWorkOrderService {
 
     @Resource
     private PMFeignApi pmFeignApi;
-
-    @Override
-    public String testMethod(String testName) {
-        return "你好," + testName;
-    }
-
-    @Override
-    public String syncMaterial(List<BaseMaterial> baseMaterials) {
-
-        if(baseMaterials.size() <= 0){
-            return "fail";
-        }
-        return "success";
-    }
 
     @Override
     public String findWorkOrder(SearchMesPmWorkOrder searchMesPmWorkOrder) {
@@ -41,7 +26,9 @@ public class MaterialServiceImpl implements MaterialService {
         if(StringUtils.isEmpty(searchMesPmWorkOrder)){
             return "fail";
         }
+
         ResponseEntity<List<MesPmWorkOrderDto>> workOrderList = pmFeignApi.findWorkOrderList(searchMesPmWorkOrder);
         return "success"+ workOrderList.getData().toString();
     }
+
 }
