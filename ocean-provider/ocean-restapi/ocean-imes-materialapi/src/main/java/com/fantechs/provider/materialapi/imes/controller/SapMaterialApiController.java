1765d36903af7b344ecac4a28af38718fd7bb660
chipengfei <819474118@qq.com>
2021-06-04 12:01:52 +0800

    删除测试类，添加雷赛物料接口

--- ocean-provider/ocean-restapi/ocean-imes-materialapi/src/main/java/com/fantechs/provider/materialapi/imes/controller/SapMaterialApiController.java
+++ ocean-provider/ocean-restapi/ocean-imes-materialapi/src/main/java/com/fantechs/provider/materialapi/imes/controller/SapMaterialApiController.java
@@ -3,17 +3,17 @@ package com.fantechs.provider.materialapi.imes.controller;
 import com.fantechs.common.base.general.dto.restapi.SearchSapMaterialApi;
 import com.fantechs.common.base.response.ControllerUtil;
 import com.fantechs.common.base.response.ResponseEntity;
-import com.fantechs.provider.materialapi.imes.service.GetTestService;
 import com.fantechs.provider.materialapi.imes.service.SapMaterialApiService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.rmi.RemoteException;
 
 /**
  *
