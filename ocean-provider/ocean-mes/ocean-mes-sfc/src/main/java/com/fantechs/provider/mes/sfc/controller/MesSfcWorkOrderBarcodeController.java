df0f52cba6934923476a19299dffd83808f5f73d
雷秦林 <lql@fantechs.com.cn>
2021-06-18 11:46:17 +0800

    sfc

--- ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/controller/MesSfcWorkOrderBarcodeController.java
+++ ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/controller/MesSfcWorkOrderBarcodeController.java
@@ -53,7 +53,7 @@ public class MesSfcWorkOrderBarcodeController {
     @ApiOperation("打印/补打")
     @PostMapping("/print")
     public ResponseEntity print(@ApiParam(value = "对象ID列表，多个逗号分隔",required = true) @RequestParam @NotBlank(message="ids不能为空") String ids,
-                                @ApiParam(value = "打印类型（1，打印，2，补打）",required = true)@RequestParam Byte printType,@RequestParam(value = "打印名称") String printName){
+                                @ApiParam(value = "打印类型（1，打印，2，补打）",required = true)@RequestParam Byte printType,@RequestParam String printName){
         return ControllerUtil.returnCRUD(mesSfcWorkOrderBarcodeService.print(ids,printType,printName));
     }
 
