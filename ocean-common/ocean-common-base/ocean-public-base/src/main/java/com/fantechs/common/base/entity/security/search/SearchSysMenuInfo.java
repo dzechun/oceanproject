62d43ce0a9672ef85dbfc676de669eee2fb913e1
‘jiangbinbin <idealLife2020@163.com>
2021-07-07 19:15:36 +0800

    菜单管理添加查询

--- ocean-common/ocean-common-base/ocean-public-base/src/main/java/com/fantechs/common/base/entity/security/search/SearchSysMenuInfo.java
+++ ocean-common/ocean-common-base/ocean-public-base/src/main/java/com/fantechs/common/base/entity/security/search/SearchSysMenuInfo.java
@@ -23,4 +23,22 @@ public class SearchSysMenuInfo extends BaseQuery implements Serializable {
      */
     @ApiModelProperty(name = "menuName",value = "菜单名称")
     private String menuName;
+
+    /**
+     * 菜单路径
+     */
+    @ApiModelProperty(name = "premenuId",value = "菜单路径")
+    private String premenuId;
+
+    /**
+     * 父级ID
+     */
+    @ApiModelProperty(name = "parentId",value = "菜单编码")
+    private Integer parentId;
+
+    /**
+     * 菜单类型
+     */
+    @ApiModelProperty(name = "menuType",value = "菜单类型")
+    private Integer menuType;
 }
