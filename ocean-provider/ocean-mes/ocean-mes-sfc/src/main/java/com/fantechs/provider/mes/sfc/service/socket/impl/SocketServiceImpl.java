afecc8ecb7235a0a607daffd0bf09f1614ed50bd
dengxinkun <dengxinkun@qq.com>
2021-07-22 10:58:25 +0800

    TCP数据采集服务

--- ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/service/socket/impl/SocketServiceImpl.java
+++ ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/service/socket/impl/SocketServiceImpl.java
@@ -1,30 +1,24 @@
 package com.fantechs.provider.mes.sfc.service.socket.impl;
 
-import com.fantechs.common.base.exception.BizErrorException;
-import com.fantechs.common.base.general.dto.eam.EamEquipmentDto;
 import com.fantechs.common.base.general.entity.eam.EamEquipment;
-import com.fantechs.common.base.general.entity.eam.search.SearchEamEquipment;
 import com.fantechs.common.base.general.entity.mes.sfc.MesSfcDataCollect;
-import com.fantechs.common.base.utils.StringUtils;
 import com.fantechs.provider.api.eam.EamFeignApi;
 import com.fantechs.provider.mes.sfc.service.MesSfcDataCollectService;
 import com.fantechs.provider.mes.sfc.service.socket.SocketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 import java.io.*;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.*;
 
 @Service
+@Component
 public class SocketServiceImpl implements SocketService {
     private static final Logger log = LoggerFactory.getLogger(SocketServiceImpl.class);
 
@@ -34,13 +28,27 @@ public class SocketServiceImpl implements SocketService {
     private EamFeignApi eamFeignApi;
 
     private int port = 8103;
+    private int timeOut = 1000 * 30;
+    private Map<String, Long> ipMap = new HashMap<>();
 
-    private int ticket = 30;
-    private Socket socket;
-    //定义Lock锁对象
-    Lock lock = new ReentrantLock();
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void checkIpTask() {
+        log.info("======== 定时器执行");
+        if (!ipMap.isEmpty()) {
+            Set<Map.Entry<String, Long>> entrySet = ipMap.entrySet();
+            Iterator<Map.Entry<String, Long>> iterator = entrySet.iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, Long> entry = iterator.next();
+                String key = entry.getKey();
+                Long value = entry.getValue();
+                if (System.currentTimeMillis() - value > timeOut) {
+                    updateStatus(key, (byte) 3);
+                    iterator.remove();
+                }
+            }
+        }
+    }
 
-    //    @PostConstruct
     @Override
     public void openService() throws IOException {
         //创建一个服务端socket
@@ -48,48 +56,11 @@ public class SocketServiceImpl implements SocketService {
 
         //调用accept方法等待连接,线程会阻塞状态
         log.info("=============> Socket服务已启动,等待连接");
-        new Thread(new Runnable() {
-            public void run() {
-                //调用服务器套接字对象中的方法accept()获取客户端套接字对象
-                while (true) {
-                    //添加同步锁
-                    lock.lock();
-                    if (ticket > 0) {
-                        try {
-                            //等待服务器链接 accept是阻塞的，作为服务器，需要一直等待客户端链接
-                            socket = serverSocket.accept();
-                            String ip = socket.getInetAddress().getHostAddress();
-                            int prot = socket.getPort();
-                            log.info("有客户端连接，ip" + ip + ",prot" + prot);
-                            if (socket.getInputStream() != null) {
-                                log.info("=============> socket.getPort" + socket.getPort());
-                                String jsonStr = inputStreamToString(socket);
-                                log.info("=============> json" + jsonStr);
-                                if (jsonStr != null) {
-                                    EamEquipment equipment = getEquipment(ip);
-                                    MesSfcDataCollect dataCollect = MesSfcDataCollect.builder()
-                                            .status((byte) 1)
-                                            .collectData(jsonStr)
-                                            .collectTime(new Date())
-                                            .createTime(new Date())
-                                            .isDelete((byte) 1)
-                                            .equipmentId(equipment.getEquipmentId())
-                                            .build();
-                                    mesSfcDataCollectService.save(dataCollect);
-                                    updateStatus(ip, (byte) 1);
-                                }
-                            }
-                        } catch (IOException e1) {
-                            String ip = socket.getInetAddress().getHostAddress();
-                            updateStatus(ip, (byte) 3);
-                            e1.printStackTrace();
-                        }
-                    }
-                    //释放同步锁
-                    lock.unlock();
-                }
-            }
-        }).start();
+        Socket socket = null;
+        while (true) {
+            socket = serverSocket.accept();
+            new SockerServerThread(socket).start();
+        }
     }
 
     private String inputStreamToString(Socket socket) throws IOException {
@@ -113,19 +84,19 @@ public class SocketServiceImpl implements SocketService {
             log.info("=============> 断开连接");
             String ip = socket.getInetAddress().getHostAddress();
             updateStatus(ip, (byte) 0);
-            socket.shutdownInput();
-            socket.shutdownOutput();
-            socket.close();
+//            socket.shutdownInput();
+//            socket.shutdownOutput();
+//            socket.close();
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return str;
     }
 
-    public EamEquipment getEquipment(String ip){
-        SearchEamEquipment searchEamEquipment = new SearchEamEquipment();
-        searchEamEquipment.setEquipmentIp(ip);
-        List<EamEquipmentDto> equipmentDtos = eamFeignApi.findList(searchEamEquipment).getData();
-        return equipmentDtos.get(0);
+    public EamEquipment getEquipment(String ip) {
+        EamEquipment eamEquipment = eamFeignApi.detailByIp(ip).getData();
+        return eamEquipment;
     }
 
     public int updateStatus(String ip, Byte bytes) {
@@ -133,6 +104,46 @@ public class SocketServiceImpl implements SocketService {
         eamEquipment.setOnlineStatus(bytes);
         eamFeignApi.update(eamEquipment);
         return 1;
+    }
+
+    public class SockerServerThread extends Thread {
+        Socket socket = null;
+
+        public SockerServerThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        //线程操作响应客户端请求
+        public void run() {
+            try {
+                String ip = socket.getInetAddress().getHostAddress();
+                int prot = socket.getPort();
+                log.info("有客户端连接，ip" + ip + ",prot" + prot);
+                if (socket.getInputStream() != null) {
+                    log.info("=============> socket.getPort" + socket.getPort());
+                    String jsonStr = inputStreamToString(socket);
+                    log.info("=============> json" + jsonStr);
+                    if (jsonStr != null) {
+                        EamEquipment equipment = getEquipment(ip);
+                        MesSfcDataCollect dataCollect = MesSfcDataCollect.builder()
+                                .status((byte) 1)
+                                .collectData(jsonStr)
+                                .collectTime(new Date())
+                                .createTime(new Date())
+                                .isDelete((byte) 1)
+                                .equipmentId(equipment.getEquipmentId())
+                                .build();
+                        mesSfcDataCollectService.save(dataCollect);
+                        updateStatus(ip, (byte) 1);
+                        ipMap.put(ip, System.currentTimeMillis());
+                    }
+                }
+            } catch (IOException e1) {
+                String ip = socket.getInetAddress().getHostAddress();
+                updateStatus(ip, (byte) 3);
+                e1.printStackTrace();
+            }
+        }
     }
 
 }
