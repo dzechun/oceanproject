d4ea0da3e1db66a294b431cb12d4da34539a5055
chipengfei <819474118@qq.com>
2021-07-27 09:09:45 +0800

    socket长连接优化

--- ocean-provider/ocean-eam/src/main/java/com/fantechs/provider/eam/service/socket/impl/SocketServiceImpl.java
+++ ocean-provider/ocean-eam/src/main/java/com/fantechs/provider/eam/service/socket/impl/SocketServiceImpl.java
@@ -6,10 +6,12 @@ import com.fantechs.common.base.general.entity.eam.EamEquipment;
 import com.fantechs.common.base.utils.StringUtils;
 import com.fantechs.provider.eam.mapper.EamEquipmentMapper;
 import com.fantechs.provider.eam.service.socket.SocketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import tk.mybatis.mapper.entity.Example;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.io.*;
 import java.net.InetAddress;
@@ -23,13 +25,35 @@ import java.util.*;
  */
 @Service
 public class SocketServiceImpl implements SocketService {
+    private static final Logger log = LoggerFactory.getLogger(SocketServiceImpl.class);
 
     @Resource
     private EamEquipmentMapper eamEquipmentMapper;
 
     private Hashtable hashtable = new Hashtable();
+    private int port = 9302;   //端口
+    private int ticket = 30;
+    private int timeOut = 1000 * 30;   //超时时间
+    private Map<String, Long> ipMap = new HashMap<>();
 
-
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void checkIpTask() {
+        log.info("======== 定时器执行");
+        if (!ipMap.isEmpty()) {
+            Set<Map.Entry<String, Long>> entrySet = ipMap.entrySet();
+            Iterator<Map.Entry<String, Long>> iterator = entrySet.iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, Long> entry = iterator.next();
+                String key = entry.getKey();
+                Long value = entry.getValue();
+                if (System.currentTimeMillis() - value > timeOut) {
+                    log.info("==============检测到设备断开，设备ip："+key);
+                    updateStatus(key, (byte) 3);
+                    iterator.remove();
+                }
+            }
+        }
+    }
 
     @Override
     public int instructions(String ip,String code,Object url) {
@@ -58,10 +82,9 @@ public class SocketServiceImpl implements SocketService {
     }
 
     @Override
-    //@PostConstruct
     public  void openService() throws IOException{
         //创建一个服务端socket
-        ServerSocket serverSocket=new ServerSocket(9302);
+        ServerSocket serverSocket=new ServerSocket(port);
 
         //调用accept方法等待连接,线程会阻塞状态
         System.out.println("Socket服务已启动,等待连接");
@@ -81,47 +104,44 @@ public class SocketServiceImpl implements SocketService {
         public SockerServerThread (Socket socket){
             this.socket=socket;
         }
-        // 获取输入流
-        // InputStream inputStream= null;
         OutputStream os=null;
         PrintWriter out=null;
 
         //线程操作响应客户端请求
         public void run(){
+
             InetAddress addr = socket.getInetAddress();
             String ip = addr.getHostAddress();
             try {
-             //   os=socket.getOutputStream();
-             //   out =new PrintWriter(os);
+
                 //开机连接发送新闻命令
                 hashtable.put(addr.getHostAddress(),socket);
-
-                System.out.println("-----ip----"+ip);
-
-                    Map<String, Object> map = new HashMap();
-                    Map<String, Object> newMap = new HashMap();
-                    Map<String, Object> newData = new HashMap();
-                    List<Map<String, Object>> newList = new ArrayList<Map<String, Object>>();
-                    map.put("code", 1201);
-                    map.put("url", "http://192.168.204.163/#/ESOPDataShow?ip=" + addr.getHostAddress());
-                    newList.add(map);
-                    newMap.put("code", 1202);
-                    newMap.put("url", "http://192.168.204.163/#/YunZhiESOP?ip=" + addr.getHostAddress());
-                    newList.add(newMap);
-                    newData.put("data", newList);
-                    String outMsg = JSON.toJSONString(newData);
-                //平板发送数据测试,循环一周时间
-                for(int i=0;i<1000000;i++) {
-                    sleep(5000);
-                    os=socket.getOutputStream();
-                    out =new PrintWriter(os);
-                    out.write(outMsg);
-                    out.flush();
-                    updateStatus(ip, (byte) 1);
-               }
-
+             //   updateStatus(ip,(byte)1);
+                Map<String, Object> map = new HashMap();
+                Map<String, Object> newMap = new HashMap();
+                Map<String, Object> newData = new HashMap();
+                List<Map<String, Object>> newList = new ArrayList<Map<String, Object>>();
+                map.put("code", 1201);
+                map.put("url", "http://192.168.204.163/#/ESOPDataShow?ip=" + addr.getHostAddress());
+                newList.add(map);
+                newMap.put("code", 1202);
+                newMap.put("url", "http://192.168.204.163/#/YunZhiESOP?ip=" + addr.getHostAddress());
+                newList.add(newMap);
+                newData.put("data", newList);
+                String outMsg = JSON.toJSONString(newData);
+                os=socket.getOutputStream();
+                out =new PrintWriter(os);
+                out.write(outMsg);
+                out.flush();
                 //读取输入字段，判断是否断开
-                inputStreamToString(socket, addr.getHostAddress());
+                while(true) {
+                    String str = inputStreamToString(socket, addr.getHostAddress());
+                    if( str!= null ){
+                        ipMap.put(ip, System.currentTimeMillis());
+                    }
+                    if("1205".equals(str))
+                        updateStatus(ip, (byte)2);
+                }
 
             } catch (Exception e) {
                 updateStatus(ip,(byte)3);
@@ -137,31 +157,22 @@ public class SocketServiceImpl implements SocketService {
         InputStreamReader inputStreamReader;
         String str = null;
         try {
-            inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             byte [] b = new byte[1024];
             int x = inputStream.read(b, 0, b.length);
             str = new String(b,0,x);
-
-            // 释放资源
-            bufferedReader.close();
-            inputStreamReader.close();
-            inputStream.close();
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("---断开连接----");
+            log.info("---断开连接----");
             updateStatus(ip, (byte) 0);
             socket.shutdownInput();
             socket.shutdownOutput();
             socket.close();
             hashtable.remove(ip);
             e.printStackTrace();
-            return null;
         }
         return str;
     }
-
 
 
 
