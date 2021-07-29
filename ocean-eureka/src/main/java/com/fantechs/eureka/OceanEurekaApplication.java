118bfd02143ce7ddc5ccc206972b901a8b12acc6
dengxinkun <dengxinkun@qq.com>
2021-07-29 18:10:56 +0800

    注册中心还原

---
+++ ocean-eureka/src/main/java/com/fantechs/eureka/OceanEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.fantechs.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class OceanEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanEurekaApplication.class, args);
+    }
+
+}
