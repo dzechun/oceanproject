6b6c944d75c023e449989d4e854aa3ba1d49c236
dengxinkun <dengxinkun@qq.com>
2021-08-19 11:26:32 +0800

    “gitignore提交删除DAQ模块依赖”

---
+++ ocean-provider/ocean-restapi/ocean-wanbao-api/src/main/java/com/fantechs/provider/wanbao/api/config/DBProperties.java
@@ -0,0 +1,15 @@
+package com.fantechs.provider.wanbao.api.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "spring.datasource")
+public class DBProperties {
+    private HikariDataSource primary;
+    private HikariDataSource secondary;
+}
---
+++ ocean-provider/ocean-restapi/ocean-wanbao-api/src/main/java/com/fantechs/provider/wanbao/api/service/SyncDataService.java
@@ -0,0 +1,28 @@
+package com.fantechs.provider.wanbao.api.service;
+
+public interface SyncDataService {
+
+    /**
+     * 万宝-物料基础信息同步
+     * @return
+     */
+    void syncMaterialData();
+
+    /**
+     * 万宝-工单信息同步
+     * @return
+     */
+    void syncOrderData();
+
+    /**
+     * 万宝-销售订单信息同步
+     * @return
+     */
+    void syncSaleOrderData();
+
+    /**
+     * 万宝-出货通知单信息同步
+     * @return
+     */
+    void syncOutDeliveryData();
+}
