879887cda74fa38f6bc096d2d2546afded1e7b9b
dengxinkun <dengxinkun@qq.com>
2021-08-20 09:44:14 +0800

    数据采集接口变更返回值

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/daq/DaqDataCollectModel.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/daq/DaqDataCollectModel.java
@@ -1,19 +1,18 @@
 package com.fantechs.common.base.general.dto.daq;
 
-import com.fantechs.common.base.general.entity.daq.DaqEquipment;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class DaqDataCollectModel implements Serializable {
 
     @ApiModelProperty(value = "设备信息")
-    private DaqEquipment eamEquipment;
-
     private List<String> tableName;
 
-    private List<String> collectDate;
+    @ApiModelProperty(value = "设备信息")
+    private Map<String, Object> data;
 }
