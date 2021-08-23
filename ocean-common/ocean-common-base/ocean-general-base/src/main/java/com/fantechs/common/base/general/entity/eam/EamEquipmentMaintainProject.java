c31e974ce7a3088171fc967a8f9077cf8ae08cc7
dengxinkun <dengxinkun@qq.com>
2021-08-23 10:04:54 +0800

    设备管理的点检项目、点检单、保养项目、保养单

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/eam/EamEquipmentMaintainProject.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/eam/EamEquipmentMaintainProject.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;orderStatus
+import java.util.List;
 
 ;
 ;
