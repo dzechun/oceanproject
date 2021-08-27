57cb95832c6eef01df39d127084ed1e54611ae17
leifengzhi <ll123456>
2021-08-27 10:47:22 +0800

    登录模块优化

--- ocean-provider/ocean-security/src/main/java/com/fantechs/security/OceanSecurityApplication.java
+++ ocean-provider/ocean-security/src/main/java/com/fantechs/security/OceanSecurityApplication.java
@@ -1,16 +1,10 @@
 package com.fantechs.security;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.converter.HttpMessageConverter;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @EnableDiscoveryClient
@@ -24,13 +18,13 @@ public class OceanSecurityApplication {
         SpringApplication.run(OceanSecurityApplication.class, args);
     }
 
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-        HttpMessageConverter<?> converter = fastConverter;
-        return new HttpMessageConverters(converter);
-    }
+//    @Bean
+//    public HttpMessageConverters fastJsonHttpMessageConverters() {
+//        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+//        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+//        fastConverter.setFastJsonConfig(fastJsonConfig);
+//        HttpMessageConverter<?> converter = fastConverter;
+//        return new HttpMessageConverters(converter);
+//    }
 }
