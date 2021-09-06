d1fa68c80fc1a612aaebf60654ac2cb06a5f2e89
dengxinkun <dengxinkun@qq.com>
2021-09-06 21:02:12 +0800

    万宝同步

--- ocean-provider/ocean-base/src/main/java/com/fantechs/provider/base/service/BaseProductModelService.java
+++ ocean-provider/ocean-base/src/main/java/com/fantechs/provider/base/service/BaseProductModelService.java
@@ -4,7 +4,6 @@ package com.fantechs.provider.base.service;
 
 import com.fantechs.common.base.general.dto.basic.imports.BaseProductModelImport;
 import com.fantechs.common.base.general.entity.basic.BaseProductModel;
-import com.fantechs.common.base.general.entity.basic.search.SearchBaseProductModel;
 import com.fantechs.common.base.support.IService;
 
 import java.util.List;
@@ -14,4 +13,11 @@ public interface BaseProductModelService extends IService<BaseProductModel> {
 
     List<BaseProductModel> selectProductModels(Map<String, Object> map);
     Map<String, Object> importExcel(List<BaseProductModelImport> baseProductModelImports);
+
+    /**
+     * 保存并返回对象
+     * @param productModel
+     * @return
+     */
+    BaseProductModel addForReturn(BaseProductModel productModel);
 }
---
+++ ocean-provider/ocean-restapi/ocean-wanbao-api/src/main/java/com/fantechs/provider/wanbao/api/mapper/MiddleMaterialMapper.java
@@ -0,0 +1,18 @@
+package com.fantechs.provider.wanbao.api.mapper;
+
+import com.fantechs.common.base.mybatis.MyMapper;
+import com.fantechs.provider.wanbao.api.entity.MiddleMaterial;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface MiddleMaterialMapper extends MyMapper<MiddleMaterial> {
+
+    /**
+     * 万宝-物料基础信息查询
+     * @return
+     */
+    List<MiddleMaterial> findMaterialData(Map<String, Object> map);
+}
