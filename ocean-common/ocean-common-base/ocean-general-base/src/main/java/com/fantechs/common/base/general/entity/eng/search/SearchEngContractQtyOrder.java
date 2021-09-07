2ada78f7a5253fd999088ee7b041e09c50d0a7b6
huangshuijun <huangshuijun@fantechs.com.cn>
2021-09-07 09:18:54 +0800

    合同量单加供应商权限控制

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/eng/search/SearchEngContractQtyOrder.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/eng/search/SearchEngContractQtyOrder.java
@@ -56,4 +56,16 @@ public class SearchEngContractQtyOrder extends BaseQuery implements Serializable
      */
     @ApiModelProperty(name="supplierName",value = "供应商名称")
     private String supplierName;
+
+    /**
+     * 备注
+     */
+    @ApiModelProperty(name="remark",value = "备注")
+    private String remark;
+
+    /**
+     * 采购量
+     */
+    @ApiModelProperty(name="purQty",value = "采购量")
+    private String purQty;
 }
