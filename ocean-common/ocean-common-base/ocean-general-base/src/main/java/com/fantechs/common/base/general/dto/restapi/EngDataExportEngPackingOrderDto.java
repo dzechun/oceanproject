9f79370c62a9eb8973935ee111f66621461dfc84
huangshuijun <huangshuijun@fantechs.com.cn>
2021-09-08 20:45:12 +0800

    五环装箱清单回写

---
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/restapi/EngDataExportEngPackingOrderDto.java
@@ -0,0 +1,77 @@
+package com.fantechs.common.base.general.dto.restapi;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+public class EngDataExportEngPackingOrderDto implements Serializable {
+
+    @Id
+    @ApiModelProperty(name="option2",value = "PSGUID")
+    @Column(name = "option2")
+    private String option2;
+
+    @ApiModelProperty(name="materialCode",value = "材料编码")
+    @Column(name = "material_code")
+    private String materialCode;
+
+    @ApiModelProperty(name="locationNum",value = "位号")
+    @Column(name = "location_num")
+    private String locationNum;
+
+    @ApiModelProperty(name="designQty",value = "设计量")
+    @Column(name = "design_qty")
+    private String designQty;
+
+    @ApiModelProperty(name="surplusQty",value = "余量")
+    @Column(name = "surplus_qty")
+    private String surplusQty;
+
+    @ApiModelProperty(name="purchaseReqQty",value = "请购量")
+    @Column(name = "purchase_req_qty")
+    private String purchaseReqQty;
+
+    @ApiModelProperty(name="chQty",value = "采购量")
+    @Column(name = "ch_qty")
+    private String chQty;
+
+    @ApiModelProperty(name="dominantTermCode",value = "主项号")
+    @Column(name = "dominant_term_code")
+    private String dominantTermCode;
+
+    @ApiModelProperty(name="deviceCode",value = "装置号")
+    @Column(name = "device_code")
+    private String deviceCode;
+
+    @ApiModelProperty(name="materialPurpose",value = "材料用途")
+    @Column(name = "material_purpose")
+    private String materialPurpose;
+
+    @ApiModelProperty(name="remark",value = "备注")
+    @Column(name = "remark")
+    private String remark;
+
+    @ApiModelProperty(name="recordTime",value = "登记时间")
+    @Column(name = "record_time")
+    private String recordTime;
+
+    @ApiModelProperty(name="recordUser",value = "登记人")
+    @Column(name = "record_user")
+    private String recordUser;
+
+    @ApiModelProperty(name="materialName",value = "货物名称")
+    @Column(name = "material_name")
+    private String materialName;
+
+    @ApiModelProperty(name="spec",value = "规格")
+    @Column(name = "spec")
+    private String spec;
+
+    @ApiModelProperty(name="unitName",value = "单位")
+    @Column(name = "unit_name")
+    private String unitName;
+}
---
+++ ocean-provider/ocean-guest/ocean-guest-eng/src/main/java/com/fantechs/provider/guest/eng/service/impl/EngDataExportEngPackingOrderServiceImpl.java
@@ -0,0 +1,62 @@
+package com.fantechs.provider.guest.eng.service.impl;
+
+import com.fantechs.common.base.general.dto.restapi.EngDataExportEngPackingOrderDto;
+import com.fantechs.common.base.response.ResponseEntity;
+import com.fantechs.common.base.support.BaseService;
+import com.fantechs.common.base.utils.JsonUtils;
+import com.fantechs.provider.api.guest.fivering.FiveringFeignApi;
+import com.fantechs.provider.guest.eng.mapper.EngDataExportEngPackingOrderMapper;
+import com.fantechs.provider.guest.eng.service.EngDataExportEngPackingOrderService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * Created by leifengzhi on 2021/09/01.
+ */
+@Service
+public class EngDataExportEngPackingOrderServiceImpl extends BaseService<EngDataExportEngPackingOrderDto> implements EngDataExportEngPackingOrderService {
+
+    @Resource
+    private EngDataExportEngPackingOrderMapper engDataExportEngPackingOrderMapper;
+    @Resource
+    FiveringFeignApi fiveringFeignApi;
+
+    @Override
+    public List<EngDataExportEngPackingOrderDto> findExportData(Map<String, Object> map) {
+        return engDataExportEngPackingOrderMapper.findExportData(map);
+    }
+
+    @Override
+    public String writePackingLists() {
+        String jsonVoiceArray="";
+        String projectID="3919";
+        Map<String, Object> map=new HashMap<>();
+        map.put("pageSize",1000);
+        List<EngDataExportEngPackingOrderDto> listDto=findExportData(map);
+        jsonVoiceArray= JsonUtils.objectToJson(listDto);
+        String s0=jsonVoiceArray.replaceAll("option2","PSGUID");
+        String s1=s0.replaceAll("materialCode","材料编码");
+        String s2=s1.replaceAll("locationNum","位号");
+        String s3=s2.replaceAll("designQty","设计量");
+        String s4=s3.replaceAll("surplusQty","余量");
+        String s5=s4.replaceAll("purchaseReqQty","请购量");
+        String s6=s5.replaceAll("chQty","采购量");
+        String s7=s6.replaceAll("dominantTermCode","主项号");
+        String s8=s7.replaceAll("deviceCode","装置号");
+        String s9=s8.replaceAll("materialPurpose","材料用途");
+        String s10=s9.replaceAll("remark","备注");
+        String s11=s10.replaceAll("recordTime","登记时间");
+        String s12=s11.replaceAll("recordUser","登记人");
+        String s13=s12.replaceAll("materialName","货物名称");
+        String s14=s13.replaceAll("spec","规格");
+        String s15=s14.replaceAll("unitName","单位");
+
+        ResponseEntity<String>  responseEntityResult=fiveringFeignApi.writePackingLists(s15,projectID);
+        return responseEntityResult.getData();
+    }
+}
