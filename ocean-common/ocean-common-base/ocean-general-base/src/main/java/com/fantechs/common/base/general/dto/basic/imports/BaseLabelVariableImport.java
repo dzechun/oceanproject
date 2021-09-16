f9aacd214c15039b2a54964b17a46f13dc2250c6
liangzhongwu <m15521035747@163.com>
2021-09-16 18:52:30 +0800

    标签变量信息

---
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/basic/imports/BaseLabelVariableImport.java
@@ -0,0 +1,248 @@
+package com.fantechs.common.base.general.dto.basic.imports;
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+@Data
+public class BaseLabelVariableImport implements Serializable {
+
+    /**
+     * 标签类别编码(必填)
+     */
+    @Excel(name = "标签类别编码(必填)",  height = 20, width = 30)
+    @ApiModelProperty(name="labelCategoryCode" ,value="标签类别编码(必填)")
+    private String labelCategoryCode;
+
+    /**
+     * 标签类别id
+     */
+    @ApiModelProperty(name="labelCategoryId" ,value="标签类别id")
+    private Long labelCategoryId;
+
+    /**
+     * 物料编码(必填)
+     */
+    @Excel(name = "物料编码(必填)", height = 20, width = 30)
+    @ApiModelProperty(name="materialCode" ,value="物料编码(必填)")
+    private String materialCode;
+
+    /**
+     * 物料id
+     */
+    @ApiModelProperty(name="materialId" ,value="物料id")
+    private Long materialId;
+
+    /**
+     * value1
+     */
+    @Excel(name = "value1", height = 20, width = 30)
+    @ApiModelProperty(name="value1" ,value="value1")
+    private String value1;
+
+    /**
+     * value2
+     */
+    @Excel(name = "value2", height = 20, width = 30)
+    @ApiModelProperty(name="value2" ,value="value2")
+    private String value2;
+
+    /**
+     * value3
+     */
+    @Excel(name = "value3", height = 20, width = 30)
+    @ApiModelProperty(name="value3" ,value="value3")
+    private String value3;
+
+    /**
+     * value4
+     */
+    @Excel(name = "value4", height = 20, width = 30)
+    @ApiModelProperty(name="value4" ,value="value4")
+    private String value4;
+
+    /**
+     * value5
+     */
+    @Excel(name = "value5", height = 20, width = 30)
+    @ApiModelProperty(name="value5" ,value="value5")
+    private String value5;
+
+    /**
+     * value6
+     */
+    @Excel(name = "value6", height = 20, width = 30)
+    @ApiModelProperty(name="value6" ,value="value6")
+    private String value6;
+
+    /**
+     * value7
+     */
+    @Excel(name = "value7", height = 20, width = 30)
+    @ApiModelProperty(name="value7" ,value="value7")
+    private String value7;
+
+    /**
+     * value8
+     */
+    @Excel(name = "value8", height = 20, width = 30)
+    @ApiModelProperty(name="value8" ,value="value8")
+    private String value8;
+
+    /**
+     * value9
+     */
+    @Excel(name = "value9", height = 20, width = 30)
+    @ApiModelProperty(name="value9" ,value="value9")
+    private String value9;
+
+    /**
+     * value10
+     */
+    @Excel(name = "value10", height = 20, width = 30)
+    @ApiModelProperty(name="value10" ,value="value10")
+    private String value10;
+
+    /**
+     * value11
+     */
+    @Excel(name = "value11", height = 20, width = 30)
+    @ApiModelProperty(name="value11" ,value="value11")
+    private String value11;
+
+    /**
+     * value12
+     */
+    @Excel(name = "value12", height = 20, width = 30)
+    @ApiModelProperty(name="value12" ,value="value12")
+    private String value12;
+
+    /**
+     * value13
+     */
+    @Excel(name = "value13", height = 20, width = 30)
+    @ApiModelProperty(name="value13" ,value="value13")
+    private String value13;
+
+    /**
+     * value14
+     */
+    @Excel(name = "value14", height = 20, width = 30)
+    @ApiModelProperty(name="value14" ,value="value14")
+    private String value14;
+
+    /**
+     * value15
+     */
+    @Excel(name = "value15", height = 20, width = 30)
+    @ApiModelProperty(name="value15" ,value="value15")
+    private String value15;
+
+    /**
+     * value16
+     */
+    @Excel(name = "value16", height = 20, width = 30)
+    @ApiModelProperty(name="value16" ,value="value16")
+    private String value16;
+
+    /**
+     * value17
+     */
+    @Excel(name = "value17", height = 20, width = 30)
+    @ApiModelProperty(name="value17" ,value="value17")
+    private String value17;
+
+    /**
+     * value18
+     */
+    @Excel(name = "value18", height = 20, width = 30)
+    @ApiModelProperty(name="value18" ,value="value18")
+    private String value18;
+
+    /**
+     * value19
+     */
+    @Excel(name = "value19", height = 20, width = 30)
+    @ApiModelProperty(name="value19" ,value="value19")
+    private String value19;
+
+    /**
+     * value20
+     */
+    @Excel(name = "value20", height = 20, width = 30)
+    @ApiModelProperty(name="value20" ,value="value20")
+    private String value20;
+
+    /**
+     * value21
+     */
+    @Excel(name = "value21", height = 20, width = 30)
+    @ApiModelProperty(name="value21" ,value="value21")
+    private String value21;
+
+    /**
+     * value22
+     */
+    @Excel(name = "value22", height = 20, width = 30)
+    @ApiModelProperty(name="value22" ,value="value22")
+    private String value22;
+
+    /**
+     * value23
+     */
+    @Excel(name = "value23", height = 20, width = 30)
+    @ApiModelProperty(name="value23" ,value="value23")
+    private String value23;
+
+    /**
+     * value24
+     */
+    @Excel(name = "value24", height = 20, width = 30)
+    @ApiModelProperty(name="value24" ,value="value24")
+    private String value24;
+
+    /**
+     * value25
+     */
+    @Excel(name = "value25", height = 20, width = 30)
+    @ApiModelProperty(name="value25" ,value="value25")
+    private String value25;
+
+    /**
+     * value26
+     */
+    @Excel(name = "value26", height = 20, width = 30)
+    @ApiModelProperty(name="value26" ,value="value26")
+    private String value26;
+
+    /**
+     * value27
+     */
+    @Excel(name = "value27", height = 20, width = 30)
+    @ApiModelProperty(name="value27" ,value="value27")
+    private String value27;
+
+    /**
+     * value28
+     */
+    @Excel(name = "value28", height = 20, width = 30)
+    @ApiModelProperty(name="value28" ,value="value28")
+    private String value28;
+
+    /**
+     * value29
+     */
+    @Excel(name = "value29", height = 20, width = 30)
+    @ApiModelProperty(name="value29" ,value="value29")
+    private String value29;
+
+    /**
+     * value30
+     */
+    @Excel(name = "value30", height = 20, width = 30)
+    @ApiModelProperty(name="value30" ,value="value30")
+    private String value30;
+}
