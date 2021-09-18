af17cae8fe4e50a5c8b6d395c467289edf654d04
huangshuijun <huangshuijun@fantechs.com.cn>
2021-09-18 09:49:29 +0800

    新合同量单接口

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/eng/EngContractQtyOrderAndPurOrderDto.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/eng/EngContractQtyOrderAndPurOrderDto.java
@@ -4,8 +4,8 @@ import com.alibaba.fastjson.annotation.JSONField;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -14,244 +14,276 @@ public class EngContractQtyOrderAndPurOrderDto implements Serializable {
     /**
      * 合同量单ID
      */
-    @Transient
-    @ApiModelProperty(name="contractQtyOrderId",value = "合同量单ID")
     @Id
+    @ApiModelProperty(name="contractQtyOrderId",value = "合同量单ID")
+    @Column(name = "contract_qty_order_id")
     private Long contractQtyOrderId;
 
     /**
      * 请购单号
      */
-    @Transient
     @ApiModelProperty(name="purchaseReqOrderCode",value = "请购单号")
+    @Column(name = "purchase_req_order_code")
     private String purchaseReqOrderCode;
 
     /**
      * 请购单名称
      */
-    @Transient
+    
     @ApiModelProperty(name="purchaseReqOrderName",value = "请购单名称")
+    @Column(name = "purchase_req_order_name")
     private String purchaseReqOrderName;
 
     /**
      * 合同号
      */
-    @Transient
+    
     @ApiModelProperty(name="contractCode",value = "合同号")
+    @Column(name = "contract_code")
     private String contractCode;
 
     /**
      * 专业编码
      */
-    @Transient
+    
     @ApiModelProperty(name="professionCode",value = "专业编码")
+    @Column(name = "profession_code")
     private String professionCode;
 
     /**
      * 专业名称
      */
-    @Transient
+    
     @ApiModelProperty(name="professionName",value = "专业名称")
+    @Column(name = "profession_name")
     private String professionName;
 
     /**
      * 材料编码
      */
-    @Transient
+    
     @ApiModelProperty(name="materialCode",value = "材料编码")
+    @Column(name = "material_code")
     private String materialCode;
 
     /**
      * 材料名称
      */
-    @Transient
+    
     @ApiModelProperty(name="materialName",value = "材料名称")
+    @Column(name = "material_name")
     private String materialName;
 
     /**
      * 设计量
      */
-    @Transient
+    
     @ApiModelProperty(name="designQty",value = "设计量")
+    @Column(name = "design_qty")
     private String designQty;
 
     /**
      * 余量
      */
-    @Transient
+    
     @ApiModelProperty(name="surplusQty",value = "余量")
+    @Column(name = "surplus_qty")
     private String surplusQty;
 
     /**
      * 请购量
      */
-    @Transient
+    
     @ApiModelProperty(name="purchaseReqQty",value = "请购量")
+    @Column(name = "purchase_req_qty")
     private String purchaseReqQty;
 
     /**
      * 采购量
      */
-    @Transient
+    
     @ApiModelProperty(name="purQty",value = "采购量")
+    @Column(name = "pur_qty")
     private String purQty;
 
     /**
      * 材料等级
      */
-    @Transient
+    
     @ApiModelProperty(name="materialGrade",value = "材料等级")
+    @Column(name = "material_grade")
     private String materialGrade;
 
     /**
      * 材料用途
      */
-    @Transient
+    
     @ApiModelProperty(name="materialPurpose",value = "材料用途")
+    @Column(name = "material_purpose")
     private String materialPurpose;
 
     /**
      * 位号
      */
-    @Transient
+    
     @ApiModelProperty(name="locationNum",value = "位号")
+    @Column(name = "location_num")
     private String locationNum;
 
     /**
      * 装置码
      */
-    @Transient
+    
     @ApiModelProperty(name="deviceCode",value = "装置码")
+    @Column(name = "device_code")
     private String deviceCode;
 
     /**
      * 主项号
      */
-    @Transient
+    
     @ApiModelProperty(name="dominantTermCode",value = "主项号")
+    @Column(name = "dominant_term_code")
     private String dominantTermCode;
 
     /**
      * 供应商ID
      */
-    @Transient
+    
     @ApiModelProperty(name="supplierId",value = "供应商ID")
+    @Column(name = "supplier_id")
     private Long supplierId;
 
     /**
      * 供应商名称
      */
-    @Transient
+    
     @ApiModelProperty(name = "supplierName",value = "供应商名称")
+    @Column(name = "supplier_name")
     private String supplierName;
 
     /**
      * 请购说明
      */
-    @Transient
+    
     @ApiModelProperty(name="purchaseReqExplain",value = "请购说明")
+    @Column(name = "purchase_req_explain")
     private String purchaseReqExplain;
 
     /**
      * 采购说明
      */
-    @Transient
+    
     @ApiModelProperty(name="purchaseExplain",value = "采购说明")
+    @Column(name = "purchase_explain")
     private String purchaseExplain;
 
     /**
      * 备注
      */
-    @Transient
+    
     @ApiModelProperty(name="remark",value = "备注")
+    @Column(name = "remark")
     private String remark;
 
 
     /**
      * 状态(0无效，1有效)
      */
-    @Transient
+    
     @ApiModelProperty(name="status",value = "状态(0无效，1有效)")
+    @Column(name = "status")
     private Byte status;
 
 
     /**
      * 组织id
      */
-    @Transient
+    
     @ApiModelProperty(name="orgId",value = "组织id")
+    @Column(name = "org_id")
     private Long orgId;
 
     /**
      * 创建人ID
      */
-    @Transient
+    
     @ApiModelProperty(name="createUserId",value = "创建人ID")
+    @Column(name = "create_user_id")
     private Long createUserId;
 
     /**
      * 创建用户名称
      */
-    @Transient
+    
     @ApiModelProperty(name = "createUserName",value = "创建用户名称")
+    @Column(name = "create_user_name")
     private String createUserName;
 
     /**
      * 创建时间
      */
-    @Transient
+    
     @ApiModelProperty(name="createTime",value = "创建时间")
     @JSONField(format ="yyyy-MM-dd HH:mm:ss")
+    @Column(name = "create_time")
     private Date createTime;
 
     /**
      * 修改人ID
      */
-    @Transient
+    
     @ApiModelProperty(name="modifiedUserId",value = "修改人ID")
+    @Column(name = "modified_user_id")
     private Long modifiedUserId;
 
     /**
      * 修改用户名称
      */
-    @Transient
+    
     @ApiModelProperty(name = "modifiedUserName",value = "修改用户名称")
+    @Column(name = "modified_user_name")
     private String modifiedUserName;
 
     /**
      * 修改时间
      */
-    @Transient
+    
     @ApiModelProperty(name="modifiedTime",value = "修改时间")
     @JSONField(format ="yyyy-MM-dd HH:mm:ss")
+    @Column(name = "modified_time")
     private Date modifiedTime;
 
     /**
      * option1
      */
-    @Transient
+    
     @ApiModelProperty(name="option1",value = "option1")
+    @Column(name = "option1")
     private String option1;
 
     /**
      * option2
      */
-    @Transient
+    
     @ApiModelProperty(name="option2",value = "option2")
+    @Column(name = "option2")
     private String option2;
 
     /**
      * option3
      */
-    @Transient
+    
     @ApiModelProperty(name="option3",value = "option3")
+    @Column(name = "option3")
     private String option3;
 
     /**
      * 组织名称
      */
-    @Transient
+    
     @ApiModelProperty(name = "organizationName",value = "组织名称")
+    @Column(name = "organization_name")
     private String organizationName;
 
 
