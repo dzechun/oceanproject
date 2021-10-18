466f177608079b380f91d66e4bad10ffabbffcfe
dengxinkun <dengxinkun@qq.com>
2021-10-18 11:28:06 +0800

    五环-移位单bug

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/mes/sfc/MesSfcBarcodeProcess.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/mes/sfc/MesSfcBarcodeProcess.java
@@ -18,6 +18,15 @@ import java.util.Date;
 ;
 ;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+;
+;
+
 /**
  * 生产管理-产品条码过站表
  * mes_sfc_barcode_process
