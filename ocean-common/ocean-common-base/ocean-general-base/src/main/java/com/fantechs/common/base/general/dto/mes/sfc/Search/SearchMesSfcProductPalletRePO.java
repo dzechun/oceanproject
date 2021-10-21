190e7b5bdfe331f39a32764a1c7ac7a2d376bae8
huangshuijun <huangshuijun@fantechs.com.cn>
2021-10-21 14:03:12 +0800

    万宝PO功能

---
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/mes/sfc/Search/SearchMesSfcProductPalletRePO.java
@@ -0,0 +1,30 @@
+package com.fantechs.common.base.general.dto.mes.sfc.Search;
+
+import com.fantechs.common.base.dto.BaseQuery;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchMesSfcProductPalletRePO extends BaseQuery implements Serializable {
+
+    /**
+     * 产品条码
+     */
+    @ApiModelProperty(name = "barcode", value = "产品条码")
+    private String barcode;
+
+    /**
+     * 销售码
+     */
+    @ApiModelProperty(name = "salesCode", value = "销售码")
+    private String salesCode;
+
+}
