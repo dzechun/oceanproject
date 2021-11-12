f4fe0f169dce5011307dc9891328bf82d5e69ff4
liangzhongwu <m15521035747@163.com>
2021-11-12 15:16:51 +0800

    查询不生效问题

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/eam/search/SearchEamEquipmentJig.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/eam/search/SearchEamEquipmentJig.java
@@ -15,4 +15,22 @@ public class SearchEamEquipmentJig extends BaseQuery implements Serializable {
     @ApiModelProperty(name="equipmentCode",value = "设备编码")
     private String equipmentCode;
 
+    /**
+     * 设备名称
+     */
+    @ApiModelProperty(name="equipmentName",value = "设备名称")
+    private String equipmentName;
+
+    /**
+     * 设备描述
+     */
+    @ApiModelProperty(name="equipmentDesc",value = "设备描述")
+    private String equipmentDesc;
+
+    /**
+     * 设备型号
+     */
+    @ApiModelProperty(name="equipmentModel",value = "设备型号")
+    private String equipmentModel;
+
 }
