f41015a39990a372f24cb29c0e18f9b14db5aede
chipengfei <819474118@qq.com>
2021-11-22 09:41:20 +0800

    雷赛日计划修改

--- ocean-provider/ocean-mes/ocean-mes-pm/src/main/java/com/fantechs/provider/mes/pm/service/impl/MesPmDailyPlanServiceImpl.java
+++ ocean-provider/ocean-mes/ocean-mes-pm/src/main/java/com/fantechs/provider/mes/pm/service/impl/MesPmDailyPlanServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -82,15 +83,29 @@ public class MesPmDailyPlanServiceImpl extends BaseService<MesPmDailyPlan> imple
         SysUser user = CurrentUserInfoUtils.getCurrentUserInfo();
         int i = 1;
         if (StringUtils.isNotEmpty(list)){
-            Example example = new Example(MesPmDailyPlan.class);
+
+            //判断当日是否有工单，有则作为插单
+            Example example2 = new Example(MesPmDailyPlan.class);
+            example2.createCriteria().andEqualTo("planTime",list.get(0).getPlanDate());
+            List<MesPmDailyPlan> mesPmDailyPlan2 = mesPmDailyPlanMapper.selectByExample(example2);
+
             List<MesPmDailyPlan> addList = new ArrayList<>();
             for (MesPmDailyPlan mesPmDailyPlan : list) {
+                Example example = new Example(MesPmDailyPlan.class);
                 example.createCriteria().andEqualTo("workOrderId",mesPmDailyPlan.getWorkOrderId() == null ? -1 : mesPmDailyPlan.getWorkOrderId());
                 MesPmDailyPlan mesPmDailyPlan1 = mesPmDailyPlanMapper.selectOneByExample(example);
-                MesPmWorkOrder mesPmWorkOrder = mesPmWorkOrderMapper.selectOneByExample(example);
 
-                mesPmDailyPlan.setScheduleQty(mesPmWorkOrder.getScheduledQty());
+                //返写工单排产数量
+                Example example1 = new Example(MesPmWorkOrder.class);
+                example1.createCriteria().andEqualTo("workOrderId",mesPmDailyPlan.getWorkOrderId() == null ? -1 : mesPmDailyPlan.getWorkOrderId());
+                MesPmWorkOrder mesPmWorkOrder = mesPmWorkOrderMapper.selectOneByExample(example1);
+                if(mesPmWorkOrder.getScheduledQty().add(mesPmDailyPlan.getScheduledQty()).compareTo(mesPmWorkOrder.getWorkOrderQty()) ==1)
+                    throw new BizErrorException("排产总数量大于工单数量");
+                mesPmWorkOrder.setScheduledQty(mesPmWorkOrder.getScheduledQty().add(mesPmDailyPlan.getScheduledQty()));
                 mesPmDailyPlan.setFinishedQty(mesPmWorkOrder.getOutputQty());
+
+                mesPmWorkOrderMapper.updateByPrimaryKeySelective(mesPmWorkOrder);
+
 
                 Date strToDate = null;
                 try {
@@ -99,18 +114,16 @@ public class MesPmDailyPlanServiceImpl extends BaseService<MesPmDailyPlan> imple
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
-                //判断当日是否有工单，有则作为插单
-                Example example1 = new Example(MesPmDailyPlan.class);
-                example1.createCriteria().andEqualTo("planTime",mesPmDailyPlan.getPlanDate());
-                List<MesPmDailyPlan> mesPmDailyPlan2 = mesPmDailyPlanMapper.selectByExample(example1);
-                if(StringUtils.isNotEmpty(mesPmDailyPlan1)) {
+
+               /* if(StringUtils.isNotEmpty(mesPmDailyPlan1)) {
                     mesPmDailyPlan.setDailyPlanId(mesPmDailyPlan1.getDailyPlanId());
                     if(StringUtils.isNotEmpty(mesPmDailyPlan2))
                         mesPmDailyPlan.setIfOrderInserting((byte)1);
                     else
                         mesPmDailyPlan.setIfOrderInserting((byte)0);
                     mesPmDailyPlan.setStatus((byte)1);
-                    mesPmDailyPlanMapper.updateByPrimaryKeySelective(mesPmDailyPlan);
+                   // mesPmDailyPlanMapper.updateByPrimaryKeySelective(mesPmDailyPlan);
+                    mesPmDailyPlanMapper.insertSelective();
                     //TODO添加到履历表
                 }else{
                     if(StringUtils.isNotEmpty(mesPmDailyPlan2))
@@ -121,8 +134,17 @@ public class MesPmDailyPlanServiceImpl extends BaseService<MesPmDailyPlan> imple
                     mesPmDailyPlan.setCreateUserId(user.getUserId());
                     mesPmDailyPlan.setModifiedUserId(user.getUserId());
                     addList.add(mesPmDailyPlan);
-                }
-                example.clear();
+                }*/
+
+                if(StringUtils.isNotEmpty(mesPmDailyPlan2))
+                    mesPmDailyPlan.setIfOrderInserting((byte)1);
+                else
+                    mesPmDailyPlan.setIfOrderInserting((byte)0);
+                mesPmDailyPlan.setStatus((byte)1);
+                mesPmDailyPlan.setCreateUserId(user.getUserId());
+                mesPmDailyPlan.setModifiedUserId(user.getUserId());
+                addList.add(mesPmDailyPlan);
+
                 }
             if(StringUtils.isNotEmpty(addList)) {
                 i = mesPmDailyPlanMapper.insertList(addList);
@@ -139,6 +161,38 @@ public class MesPmDailyPlanServiceImpl extends BaseService<MesPmDailyPlan> imple
             mesPmDailyPlan.setStatus((byte)0);
             this.update(mesPmDailyPlan);
         }
+        return 1;
+    }
+
+    @Override
+    public int batchUpdate(List<MesPmDailyPlan> list) {
+        for(MesPmDailyPlan plan : list){
+                SysUser currentUser = CurrentUserInfoUtils.getCurrentUserInfo();
+                //返写工单数据
+                Example example = new Example(MesPmDailyPlan.class);
+                Example.Criteria criteria = example.createCriteria();
+                criteria.andEqualTo("workOrderId", plan.getWorkOrderId());
+                criteria.andEqualTo("status", 1);
+                List<MesPmDailyPlan> mesPmDailyPlans = mesPmDailyPlanMapper.selectByExample(example);
+                BigDecimal qyt = BigDecimal.ZERO;
+                for (MesPmDailyPlan s : mesPmDailyPlans) {
+                    if (!s.getDailyPlanId().equals(plan.getDailyPlanId()))
+                        qyt = qyt.add(s.getScheduledQty());
+                }
+                Example example1 = new Example(MesPmWorkOrder.class);
+                Example.Criteria criteria1 = example1.createCriteria();
+                criteria1.andEqualTo("workOrderId", plan.getWorkOrderId());
+                MesPmWorkOrder workOrder = mesPmWorkOrderMapper.selectOneByExample(example1);
+                if(qyt.add(plan.getScheduledQty()).compareTo(workOrder.getWorkOrderQty()) ==1)
+                    throw new BizErrorException("排产总数量大于工单数量");
+                workOrder.setScheduledQty(qyt.add(plan.getScheduledQty()));
+                mesPmWorkOrderMapper.updateByPrimaryKeySelective(workOrder);
+
+                plan.setModifiedUserId(currentUser.getUserId());
+                plan.setModifiedTime(new Date());
+                mesPmDailyPlanMapper.updateByPrimaryKeySelective(plan);
+        }
+
         return 1;
     }
 }
