8ee7b56e4cc1e8633ce7fb5c4bdee281e453232e
xupeifeng <xupeifeng@fantechs.com.cn>
2021-12-04 14:29:18 +0800

    license证书校验

---
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/security/LicenseCheckModel.java
@@ -0,0 +1,45 @@
+package com.fantechs.common.base.general.dto.security;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 自定义需要校验的License参数
+ */
+@Data
+public class LicenseCheckModel implements Serializable{
+
+    private static final long serialVersionUID = 8600137500316662317L;
+    /**
+     * 可被允许的IP地址
+     */
+    private List<String> ipAddress;
+
+    /**
+     * 可被允许的MAC地址
+     */
+    private List<String> macAddress;
+
+    /**
+     * 可被允许的CPU序列号
+     */
+    private String cpuSerial;
+
+    /**
+     * 可被允许的主板序列号
+     */
+    private String mainBoardSerial;
+
+
+    @Override
+    public String toString() {
+        return "LicenseCheckModel{" +
+                "ipAddress=" + ipAddress +
+                ", macAddress=" + macAddress +
+                ", cpuSerial='" + cpuSerial + '\'' +
+                ", mainBoardSerial='" + mainBoardSerial + '\'' +
+                '}';
+    }
+}
---
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/dto/security/LicenseVerifyParam.java
@@ -0,0 +1,58 @@
+package com.fantechs.common.base.general.dto.security;
+
+import lombok.Data;
+
+/**
+ * License校验类需要的参数
+ */
+@Data
+public class LicenseVerifyParam {
+
+    /**
+     * 证书subject
+     */
+    private String subject;
+
+    /**
+     * 公钥别称
+     */
+    private String publicAlias;
+
+    /**
+     * 访问公钥库的密码
+     */
+    private String storePass;
+
+    /**
+     * 证书生成路径
+     */
+    private String licensePath;
+
+    /**
+     * 密钥库存储路径
+     */
+    private String publicKeysStorePath;
+
+    public LicenseVerifyParam() {
+
+    }
+
+    public LicenseVerifyParam(String subject, String publicAlias, String storePass, String licensePath, String publicKeysStorePath) {
+        this.subject = subject;
+        this.publicAlias = publicAlias;
+        this.storePass = storePass;
+        this.licensePath = licensePath;
+        this.publicKeysStorePath = publicKeysStorePath;
+    }
+
+    @Override
+    public String toString() {
+        return "LicenseVerifyParam{" +
+                "subject='" + subject + '\'' +
+                ", publicAlias='" + publicAlias + '\'' +
+                ", storePass='" + storePass + '\'' +
+                ", licensePath='" + licensePath + '\'' +
+                ", publicKeysStorePath='" + publicKeysStorePath + '\'' +
+                '}';
+    }
+}
