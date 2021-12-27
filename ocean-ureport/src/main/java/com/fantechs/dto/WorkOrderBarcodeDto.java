526deb0d68276310f34cb64514cf91d54a1ca8a1
dengxinkun <dengxinkun@qq.com>
2021-12-27 15:52:32 +0800

    万宝-生产报表修改

--- ocean-ureport/src/main/java/com/fantechs/dto/WorkOrderBarcodeDto.java
+++ ocean-ureport/src/main/java/com/fantechs/dto/WorkOrderBarcodeDto.java
@@ -13,4 +13,10 @@ public class WorkOrderBarcodeDto implements Serializable {
 
     @ApiModelProperty(name="barcodeStatus",value = "条码状态")
     private String barcodeStatus;
+
+    @ApiModelProperty(name = "salesBarcode",value = "销售条码")
+    private String salesBarcode;
+
+    @ApiModelProperty(name = "cutsomerBarcode",value = "客户条码")
+    private String cutsomerBarcode;
 }
