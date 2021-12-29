ee7512812e7bbd3623f101347986bbb123f16021
chipengfei <819474118@qq.com>
2021-12-29 20:45:27 +0800

    直接调拨单

--- ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/wms/inner/search/SearchWmsInnerDirectTransferOrder.java
+++ ocean-common/ocean-common-base/ocean-general-base/src/main/java/com/fantechs/common/base/general/entity/wms/inner/search/SearchWmsInnerDirectTransferOrder.java
@@ -10,5 +10,18 @@ import java.io.Serializable;
 @Data
 public class SearchWmsInnerDirectTransferOrder extends BaseQuery implements Serializable {
 
+    /**
+     * 直接调拨单号
+     */
+    @ApiModelProperty(name="directTransferOrderCode",value = "直接调拨单号")
+    private String directTransferOrderCode;
+
+    /**
+     * 单据状态(1-待作业、2-作业中、3-完成)
+     */
+    @ApiModelProperty(name="orderStatus",value = "单据状态(1-待作业、2-作业中、3-完成)")
+    private Byte orderStatus;
+
+
     private static final long serialVersionUID = 1L;
 }
