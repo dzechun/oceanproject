7545f9bcea76506dce44ac87d27786456247a2ee
dengxinkun <dengxinkun@qq.com>
2022-03-03 11:14:58 +0800

    栈板作业移除判断同个工单的、已过包箱过站但是没有栈板过站的包箱、并且PO号一致的数据是否存在相关逻辑

--- ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/service/impl/MesSfcPalletWorkServiceImpl.java
+++ ocean-provider/ocean-mes/ocean-mes-sfc/src/main/java/com/fantechs/provider/mes/sfc/service/impl/MesSfcPalletWorkServiceImpl.java
@@ -536,43 +536,6 @@ public class MesSfcPalletWorkServiceImpl implements MesSfcPalletWorkService {
         palletWorkScanDto.setClosePalletNum(BigDecimal.valueOf(closePalletNum));
         palletWorkScanDto.setNowPackageSpecQty(nowPackageSpecQty);
         palletWorkScanDto.setScanCartonNum(palletCartons + 1);
-
-        // 未满箱
-        if (nowPackageSpecQty.compareTo(BigDecimal.valueOf(palletCartons + 1)) != 0){
-            /**
-             * 万宝特性要求
-             * 当程序配置项有数据时，则走此段逻辑
-             * 栈板作业，增加根据产品条码、PO号判断当前产线是否有同样的产品，没有则关闭当前栈板
-             */
-            SearchSysSpecItem searchSysSpecItem = new SearchSysSpecItem();
-            searchSysSpecItem.setSpecCode("wanbaoAutoClosePallet");
-            List<SysSpecItem> specItems = securityFeignApi.findSpecItemList(searchSysSpecItem).getData();
-            if (!specItems.isEmpty() && "1".equals(specItems.get(0).getParaValue())){
-                // 判断同个工单的、已过包箱过站但是没有栈板过站的包箱、并且PO号一致的数据是否存在
-                Map<String, Object> param = new HashMap<>();
-                param.put("workOrderId", mesSfcProductPallet.getWorkOrderId());
-//                param.put("samePackageCode", );
-
-                //扫描的条码有PO号判断 2021-10-22
-                if(StringUtils.isNotEmpty(samePackageCode) && requestPalletWorkScanDto.getPalletType() == 2){
-                    param.put("samePackageCode", samePackageCode);
-                }
-
-                List<MesSfcBarcodeProcess> nextProcessIsPallet = mesSfcBarcodeProcessService.findNextProcessIsPallet(param);
-                // TODO PO号判断
-                //栈板作业的时候，判断当前条码有没有PO号，
-                //如果有PO号 -> 则校验条码工单号、PO号在已过包箱数据里面还有没有，有就不做任何处理，没有的话就自动提交当前栈板
-                //如果没有PO号 -> 则校验条码工单号在已过包箱数据里面还有没有，有就不做任何处理，没有的话就自动提交当前栈板
-
-                if (nextProcessIsPallet.isEmpty()){
-                    // 未满栈板自动提交
-                    List<Long> palletIdList = new ArrayList<>();
-                    palletIdList.add(mesSfcProductPallet.getProductPalletId());
-                    this.submitNoFullPallet(palletIdList, requestPalletWorkScanDto.getPrintBarcode(), requestPalletWorkScanDto.getPrintName(), requestPalletWorkScanDto.getProcessId());
-                }
-            }
-        }
-
         return palletWorkScanDto;
     }
 
