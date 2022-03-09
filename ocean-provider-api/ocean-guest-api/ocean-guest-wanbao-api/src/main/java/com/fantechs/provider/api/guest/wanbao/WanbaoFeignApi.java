a1f58eedbdfc9b4af597e581e0f255554eba0bb4
huangshuijun <huangshuijun@fantechs.com.cn>
2022-03-09 17:40:05 +0800

    冲突处理

--- ocean-provider-api/ocean-guest-api/ocean-guest-wanbao-api/src/main/java/com/fantechs/provider/api/guest/wanbao/WanbaoFeignApi.java
+++ ocean-provider-api/ocean-guest-api/ocean-guest-wanbao-api/src/main/java/com/fantechs/provider/api/guest/wanbao/WanbaoFeignApi.java
@@ -1,11 +1,8 @@
 package com.fantechs.provider.api.guest.wanbao;
 
-<<<<<<< HEAD
 import com.fantechs.common.base.general.entity.wanbao.QmsInspectionOrder;
 import com.fantechs.common.base.general.entity.wanbao.QmsInspectionOrderDetSample;
-=======
 import com.fantechs.common.base.general.entity.wanbao.WanbaoStacking;
->>>>>>> 121a03a1a55190e76e6bed1756973c1392d6d550
 import com.fantechs.common.base.general.entity.wanbao.WanbaoStackingDet;
 import com.fantechs.common.base.general.entity.wanbao.search.SearchQmsInspectionOrder;
 import com.fantechs.common.base.general.entity.wanbao.search.SearchQmsInspectionOrderDetSample;
@@ -28,7 +25,6 @@ public interface WanbaoFeignApi {
     @PostMapping("/wanbaoStackingDet/batchAdd")
     ResponseEntity batchAdd(@ApiParam(value = "必传：",required = true)@RequestBody @Validated List<WanbaoStackingDet> list);
 
-<<<<<<< HEAD
     @ApiOperation("页签信息列表")
     @PostMapping("/qmsInspectionOrderDetSample/findList")
     ResponseEntity<List<QmsInspectionOrderDetSample>> findList(@ApiParam(value = "查询对象")@RequestBody SearchQmsInspectionOrderDetSample searchQmsInspectionOrderDetSample);
@@ -36,7 +32,6 @@ public interface WanbaoFeignApi {
     @ApiOperation("成品检验单列表")
     @PostMapping("/qmsInspectionOrder/findList")
     ResponseEntity<List<QmsInspectionOrder>> findList(@ApiParam(value = "查询对象")@RequestBody SearchQmsInspectionOrder searchQmsInspectionOrder);
-=======
     @ApiOperation("堆垛修改")
     @PostMapping("/wanbaoStacking/update")
     ResponseEntity update(@ApiParam(value = "对象，Id必传",required = true)@RequestBody @Validated(value= WanbaoStacking.update.class) WanbaoStacking wanbaoStacking);
@@ -44,6 +39,5 @@ public interface WanbaoFeignApi {
     @ApiOperation("获取堆垛详情")
     @PostMapping("/wanbaoStacking/detail")
     ResponseEntity<WanbaoStacking> detail(@ApiParam(value = "ID",required = true)@RequestParam @NotNull(message="id不能为空") Long id);
->>>>>>> 121a03a1a55190e76e6bed1756973c1392d6d550
 
 }
