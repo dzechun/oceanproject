cb684709433e76a1696349079e558e6ad550564f
huangkeguang <keguang.huang@cyberway.net.cn>
2022-06-09 16:27:12 +0800

    解决跨域问题

--- ocean-provider/ocean-auth/src/main/java/com/fantechs/auth/satokenconfigure/SaTokenConfigure.java
+++ ocean-provider/ocean-auth/src/main/java/com/fantechs/auth/satokenconfigure/SaTokenConfigure.java
@@ -2,6 +2,7 @@ package com.fantechs.auth.satokenconfigure;
 
 import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.filter.SaServletFilter;
+import cn.dev33.satoken.router.SaHttpMethod;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
@@ -77,26 +78,20 @@ public class SaTokenConfigure implements WebMvcConfigurer, AccessDecisionManager
                 })
 
                 // 前置函数：在每次认证函数之前执行
-                .setBeforeAuth(r -> {
-                    // 设置跨域响应头
+                .setBeforeAuth(obj -> {
+                    // ---------- 设置跨域响应头 ----------
                     SaHolder.getResponse()
                             // 允许指定域访问跨域资源
                             .setHeader("Access-Control-Allow-Origin", "*")
                             // 允许所有请求方式
                             .setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
-                            // 有效时间
-                            .setHeader("Access-Control-Max-Age", "3600")
                             // 允许的header参数
-                            .setHeader("Access-Control-Allow-Headers", "*")
-                            // 服务器名称
-                            .setServer("ocean-auth")
-                            // 是否可以在iframe显示视图： DENY=不可以 | SAMEORIGIN=同域下可以 | ALLOW-FROM uri=指定域名下可以
-                            .setHeader("X-Frame-Options", "SAMEORIGIN")
-                            // 是否启用浏览器默认XSS防护： 0=禁用 | 1=启用 | 1; mode=block 启用, 并在检查到XSS攻击时，停止渲染页面
-                            .setHeader("X-XSS-Protection", "1; mode=block")
-                            // 禁用浏览器内容嗅探
-                            .setHeader("X-Content-Type-Options", "nosniff")
-                    ;
+                            .setHeader("Access-Control-Allow-Headers", "*");
+
+                    // 如果是预检请求，则立即返回到前端
+                    SaRouter.match(SaHttpMethod.OPTIONS)
+                            .free(r -> System.out.println("--------OPTIONS预检请求，不做处理"))
+                            .back();
                 });
     }
 
