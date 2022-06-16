0a9c001ef7d54bd6f171242e89b81c2dc444e7b3
huangkeguang <keguang.huang@cyberway.net.cn>
2022-06-16 18:37:25 +0800

    nacos全局配置切换

--- ocean-provider/ocean-security/src/main/java/com/fantechs/security/mapper/SysUserRoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.fantechs.security.mapper;
-
-
-import com.fantechs.common.base.entity.security.SysUserRole;
-import com.fantechs.common.base.mybatis.MyMapper;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SysUserRoleMapper extends MyMapper<SysUserRole> {
-}
