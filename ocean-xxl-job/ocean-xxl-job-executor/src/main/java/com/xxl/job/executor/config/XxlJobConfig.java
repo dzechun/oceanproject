0309c153f79ece84d19004de5b10a751570dafc6
huangkeguang <keguang.huang@cyberway.net.cn>
2022-06-27 13:41:12 +0800

    xxl-job maven配置调整

--- ocean-xxl-job/ocean-xxl-job-executor/src/main/java/com/xxl/job/executor/config/XxlJobConfig.java
+++ ocean-xxl-job/ocean-xxl-job-executor/src/main/java/com/xxl/job/executor/config/XxlJobConfig.java
@@ -53,7 +53,6 @@ public class XxlJobConfig {
         xxlJobSpringExecutor.setAccessToken(accessToken);
         xxlJobSpringExecutor.setLogPath(logPath);
         xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
-
         return xxlJobSpringExecutor;
     }
 }
