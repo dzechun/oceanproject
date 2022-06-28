4f1f3c53d1e89098b7a0c3fb9c0df66fcea47c1a
huangkeguang <keguang.huang@cyberway.net.cn>
2022-06-28 09:55:01 +0800

    调整定时任务配置

---
+++ ocean-xxl-job/ocean-xxl-job-executor/src/main/java/com/xxl/job/executor/jobhandler/XxlJobTest.java
@@ -0,0 +1,37 @@
+package com.xxl.job.executor.jobhandler;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * XxlJob开发示例（Bean模式）
+ *
+ * 开发步骤：
+ *      1、任务开发：在Spring Bean实例中，开发Job方法；
+ *      2、注解配置：为Job方法添加注解 "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"，注解value值对应的是调度中心新建任务的JobHandler属性的值。
+ *      3、执行日志：需要通过 "XxlJobHelper.log" 打印执行日志；
+ *      4、任务结果：默认任务结果为 "成功" 状态，不需要主动设置；如有诉求，比如设置任务结果为失败，可以通过 "XxlJobHelper.handleFail/handleSuccess" 自主设置任务结果；
+ *
+ * @author xuxueli 2019-12-11 21:52:51
+ */
+@Component
+public class XxlJobTest {
+    private static Logger logger = LoggerFactory.getLogger(XxlJobTest.class);
+
+    /**
+     * 1、简单任务示例（Bean模式）
+     */
+    @XxlJob("testHandle")
+    public ReturnT<String> testHandle() throws Exception {
+        for (int i = 0; i < 5; i++) {
+            XxlJobHelper.log("beat at:" + i);
+        }
+        return ReturnT.SUCCESS;
+    }
+}
