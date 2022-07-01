d7bc3f6a2b9670f78ec4ae595eefa4e936d3eb70
zhoujiayao <zhou.jiayao@outlook.com>
2022-07-01 11:17:28 +0800

    pom调整

--- ocean-provider/ocean-base/src/main/java/com/fantechs/provider/base/OceanBaseApplication.java
+++ ocean-provider/ocean-base/src/main/java/com/fantechs/provider/base/OceanBaseApplication.java
@@ -3,12 +3,13 @@ package com.fantechs.provider.base;
 import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @ComponentScan({"com.fantechs.provider.base","com.fantechs.common"})
 @MapperScan({"com.fantechs.provider.base.mapper"})
