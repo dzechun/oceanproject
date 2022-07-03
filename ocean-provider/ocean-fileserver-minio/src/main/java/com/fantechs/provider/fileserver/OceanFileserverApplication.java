95e9fbe4745e554a582c0db7abd8f77125b2a653
jiayao <zhou.jiayao@outlook.com>
2022-07-04 00:18:26 +0800

    调整项目打包方式；集成minio测试中

---
+++ ocean-provider/ocean-fileserver-minio/src/main/java/com/fantechs/provider/fileserver/OceanFileserverApplication.java
@@ -0,0 +1,28 @@
+package com.fantechs.provider.fileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@ComponentScan({"com.fantechs.provider","com.fantechs.common"})
+public class OceanFileserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanFileserverApplication.class, args);
+    }
+
+    @GetMapping(value = "/index")
+    public String index() {
+        return "this is user index";
+    }
+
+}
---
+++ ocean-provider/ocean-fileserver-minio/src/main/java/com/fantechs/provider/fileserver/entity/ObjectItem.java
@@ -0,0 +1,10 @@
+package com.fantechs.provider.fileserver.entity;
+
+import lombok.Data;
+
+@Data
+public class ObjectItem {
+    private String objectName;
+    private Long size;
+}
+
