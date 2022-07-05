3db6555229d63a5d6980fee8d52c81e02af61223
huangkeguang <keguang.huang@cyberway.net.cn>
2022-07-05 13:27:41 +0800

    启动类移除druid类

--- ocean-xxl-job/ocean-xxl-job-executor/src/main/java/com/xxl/job/executor/OceanXxlJobExecutorApplication.java
+++ ocean-xxl-job/ocean-xxl-job-executor/src/main/java/com/xxl/job/executor/OceanXxlJobExecutorApplication.java
@@ -3,8 +3,9 @@ package com.xxl.job.executor;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,DruidDataSourceAutoConfigure.class})
 public class OceanXxlJobExecutorApplication {
 
 	public static void main(String[] args) {
