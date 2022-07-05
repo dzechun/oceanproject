4102ecf8db2ed4782a19b4ac080fe3c938501198
huangkeguang <keguang.huang@cyberway.net.cn>
2022-07-05 15:18:11 +0800

    maven配置调整，删除测试引用

--- ocean-xxl-job/ocean-xxl-job-admin/src/main/java/com/xxl/job/admin/OceanXxlJobAdminApplication.java
+++ ocean-xxl-job/ocean-xxl-job-admin/src/main/java/com/xxl/job/admin/OceanXxlJobAdminApplication.java
@@ -3,13 +3,9 @@ package com.xxl.job.admin;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-		DataSourceTransactionManagerAutoConfiguration.class,
-		DruidDataSourceAutoConfigure.class,
-		HibernateJpaAutoConfiguration.class})
+		DruidDataSourceAutoConfigure.class})
 public class OceanXxlJobAdminApplication {
 
 	public static void main(String[] args) {
